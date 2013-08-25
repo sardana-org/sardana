3e240ec44f1ea52339b50a7ab2dd061fe3d9e6bd
Picca Frédéric-Emmanuel <picca@debian.org>
2013-08-25 10:55:19 +0200

    add a standard header with the legal informations

--- src/sardana/pool/poolcontrollers/HklPseudoMotorController.py
+++ src/sardana/pool/poolcontrollers/HklPseudoMotorController.py
@@ -1,3 +1,40 @@
+##############################################################################
+##
+## This file is part of Sardana
+##
+## http://www.tango-controls.org/static/sardana/latest/doc/html/index.html
+##
+## Copyright: 2013 PetraIII,
+##            2013 Synchrotron-Soleil
+##                 L'Orme des Merisiers Saint-Aubin
+##                 BP 48 91192 GIF-sur-YVETTE CEDEX
+##
+## Sardana is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Sardana is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with Sardana.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
+
+"""This module contains the definition of an Hkl pseudo motor controller
+for the Sardana Device Pool"""
+
+__author__ = ("Maria-Teresa Nunez-Pardo-de-Verra <tnunez@mail.desy.de>, "
+              "Picca Frédéric-Emmanuel <picca@synchrotron-soleil.fr>")
+__copyright__ = ("Copyright (c) 2013 PetraIII, "
+                 "Copyright (c) 2013 Synchrotron-Soleil "
+                 "L'Orme des Merisiers Saint-Aubin "
+                 "BP 48 91192 GIF-sur-YVETTE CEDEX")
+__license__ = 'LGPL-3+'
+
 import math
 
 from sardana import State, DataAccess
