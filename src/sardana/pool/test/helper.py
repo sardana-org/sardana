cd1d9e936d5b3db9392dfebc673ef649a4e45da3
zreszela <zreszela@cells.es>
2015-01-28 14:46:26 +0100

    Add trigger/gate helper functions
    Add function to create a PoolTriggerGate from a configuration dict.
    
    Add function to create a configuration dictionary from information
    about the controllers and its channels. This configuration is required
    by the TGAction.

--- src/sardana/pool/test/helper.py
+++ src/sardana/pool/test/helper.py
@@ -24,10 +24,12 @@
 ##############################################################################
 
 __all__ = ['createPoolController', 'createPoolCounterTimer',
-           'createPoolMeasurementGroup']
+           'createPoolTriggerGate', 'createPoolMeasurementGroup',
+           'createPoolTGActionConfiguration']
 
 from sardana.pool.poolcontroller import PoolController
 from sardana.pool.poolcountertimer import PoolCounterTimer
+from sardana.pool.pooltriggergate import PoolTriggerGate
 from sardana.pool.poolmeasurementgroup import PoolMeasurementGroup
 
 def createPoolController(pool, conf):
@@ -54,9 +56,49 @@ def createPoolCounterTimer(pool, poolcontroller, conf):
     kwargs['ctrl'] = poolcontroller
     return PoolCounterTimer(**kwargs)
 
+def createPoolTriggerGate(pool, poolcontroller, conf):
+    '''Method to create a PoolTriggerGate using a configuration dictionary
+    '''
+    kwargs = conf
+    kwargs['pool'] = pool
+    kwargs['ctrl'] = poolcontroller
+    return PoolTriggerGate(**kwargs)
+
 def createPoolMeasurementGroup(pool, conf):
     '''Method to create a PoolMeasurementGroup using a configuration dictionary
     '''
     kwargs = conf
     kwargs['pool'] = pool
     return PoolMeasurementGroup(**kwargs)
+
+def createPoolTGActionConfiguration(ctrls, ctrls_conf, 
+                                    ctrl_channels, ctrl_channels_conf):
+    '''Method to create TGAction configuration. Order of the sequences is 
+    important. For all sequences, the element of a given position refers 
+    the same controller. 
+    
+    :param ctrls: sequence of the controllers used by the action
+    :type ctrls: seq<sardana.pool.PoolController>
+    :param ctrls_conf: sequence of the controllers configuration dictionaries
+    :type ctrls_conf: dict
+    :param ctrl_channels: sequence of the sequences of the channels 
+    corresponding to the controllers 
+    :type ctrl_channels: seq<seq<sardana.pool.PoolTriggerGate>>
+    :param ctrl_channels_conf: sequence of the sequences of the channels 
+    configuration dictionaries
+    :type ctrl_channels_conf: seq<seq<dict>>
+    :return: a configuration dictionary
+    :rtype: dict<>
+    '''
+
+    ctrls_configuration = {}
+    for ctrl, ctrl_conf, channels, channels_conf in zip(ctrls, ctrls_conf, 
+                                    ctrl_channels, ctrl_channels_conf):
+        ctrl_conf['units'] = {}
+        ctrl_conf['units']['0'] = main_unit_data = {}
+        ctrl_conf['units']['0']['channels'] = {}
+        for channel, channel_conf in zip(channels, channels_conf):
+            main_unit_data['channels'][channel] = channel_conf
+        ctrls_configuration[ctrl] = ctrl_conf
+    configuration = {'controllers': ctrls_configuration}
+    return configuration 
