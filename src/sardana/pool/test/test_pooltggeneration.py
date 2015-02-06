e8e8170dbd929ebd77b6e4905640904b520d8dba
zreszela <zreszela@cells.es>
2015-02-06 18:46:20 +0100

    Merge start_action and action_loop tests
    Merge start_action and action_loop tests in one tggeneration
    test. There were too much repeated code to maintain these test as
    separate.
    
    Add some comments.
    
    Use createPoolTGGenerationConfiguration instead of
    createPoolTGActionConfiguration (they were renamed in one of the
    previous comments)

--- src/sardana/pool/test/test_pooltggeneration.py
+++ src/sardana/pool/test/test_pooltggeneration.py
@@ -22,81 +22,81 @@
 ## along with Sardana.  If not, see <http://www.gnu.org/licenses/>.
 ##
 ##############################################################################
-from mock import Mock, call
-import time
 import threading
+
 from taurus.external import unittest
+#TODO: import mock using taurus.external
+from mock import Mock, call
+
 from sardana.pool.pooltggeneration import PoolTGGeneration
 from sardana.sardanadefs import State
 from sardana.pool.test import (FakePool, createPoolController,
                                createPoolTriggerGate, dummyPoolTGCtrlConf01,
                                dummyTriggerGateConf01, 
-                               createPoolTGActionConfiguration) 
+                               createPoolTGGenerationConfiguration)
 
 class PoolTriggerGateTestCase(unittest.TestCase):
-    """Unittest of PoolTGGeneration Class"""
+    """Unittest of PoolTGGeneration class"""
 
     def setUp(self):
         """Create a Controller, TriggerGate and PoolTGGeneration objects from 
-        dummy configurations"""
+        dummy configurations
+        """
         unittest.TestCase.setUp(self)
         pool = FakePool()
-
         dummy_tg_ctrl = createPoolController(pool, dummyPoolTGCtrlConf01)
         self.dummy_tg = createPoolTriggerGate(pool, dummy_tg_ctrl, 
                                                         dummyTriggerGateConf01)
         dummy_tg_ctrl.add_element(self.dummy_tg)
         pool.add_element(dummy_tg_ctrl)
         pool.add_element(self.dummy_tg)
-        self.cfg = createPoolTGActionConfiguration((dummy_tg_ctrl,), 
+        self.cfg = createPoolTGGenerationConfiguration((dummy_tg_ctrl,),
                                         (dummyTriggerGateConf01,),
                                         ((self.dummy_tg,),),
                                         ((dummyTriggerGateConf01,),))
-        
         # Create mock and define its functions
         ctrl_methods = ['PreStartAll', 'StartAll', 'PreStartOne', 'StartOne',
                         'PreStateAll', 'StateAll', 'PreStateOne', 'StateOne']
         self.mock_tg_ctrl = Mock(spec=ctrl_methods)
         self.mock_tg_ctrl.StateOne.return_value = (State.Moving, 'triggering')
+
         dummy_tg_ctrl.ctrl = self.mock_tg_ctrl
         self.tgaction = PoolTGGeneration(self.dummy_tg)
         self.tgaction.add_element(self.dummy_tg)
 
-    def test_start_action(self):
-        """Verify that the created PoolTGAction start_action starts correctly 
-        the involved controller."""
-        args = ()
-        kwargs = {'config': self.cfg}
-        self.tgaction.start_action(*args, **kwargs)
-        self.mock_tg_ctrl.assert_has_calls([call.PreStartAll(), 
-                                           (call.PreStartOne(1,)),
-                                           (call.StartOne(1,)),
-                                           (call.StartAll())])
-
-    def delaySetState(self):
-        time.sleep
+    def stopGeneration(self):
+        """Method used to change the controller (mock) state"""
         self.mock_tg_ctrl.StateOne.return_value = (State.On, 'On')
 
     def test_action_loop(self):
         """Verify trigger element states before and after action_loop."""
         args = ()
         kwargs = {'config': self.cfg}
+        # starting action
         self.tgaction.start_action(*args, **kwargs)
+        # verifying that the action correctly started the involved controller
+        self.mock_tg_ctrl.assert_has_calls([call.PreStartAll(),
+                                           (call.PreStartOne(1,)),
+                                           (call.StartOne(1,)),
+                                           (call.StartAll())])
+        # verifying that the elements involved in action changed its state
         element_state = self.dummy_tg.get_state()
         msg = ("State after start_action is '%s'. (Expected: '%s')" % 
                                     (State.get(element_state), "Moving"))
-        self.assertEqual(element_state, State.Moving, msg)       
-        threading.Timer(1, self.delaySetState).start() 
-    
+        self.assertEqual(element_state, State.Moving, msg)
+        # starting timer (1 s) which will change the controller state
+        threading.Timer(1, self.delaySetState).start()
+        # entering action loop
         self.tgaction.action_loop()
-
+        # verifying that the action checked the controller states
         self.mock_tg_ctrl.assert_has_calls([call.PreStateAll(),
                                             call.PreStateOne(1,),
                                             call.StateAll(), 
                                             call.StateOne(1,)])
+        # verifying that the elements involved in action changed its state
         element_state = self.dummy_tg.get_state()
         msg = ("State after action_loop shall be different than Moving")
-        self.assertNotEqual(element_state, State.Moving, msg)    
+        self.assertNotEqual(element_state, State.Moving, msg)
 
     def tearDown(self):
         self.tgaction = None
@@ -104,4 +104,3 @@ class PoolTriggerGateTestCase(unittest.TestCase):
         self.cfg = None
         self.dummy_tg = None
         unittest.TestCase.tearDown(self)
-
