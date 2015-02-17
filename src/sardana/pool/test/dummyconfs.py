8e1ca7bc1ecc38eabdff03694ed9bc75548858cd
Marc Josep Rosanes Siscart <mrosanes@cells.es>
2015-02-17 17:37:07 +0100

    Add helper functions for configuration dictionaries
    Add helper function to create a general MG (measurement group)
    configuration dictionary and another to create a TG (trigger/gate)
    configuration dictionary derived from the MG configuration dictionary.

--- src/sardana/pool/test/dummyconfs.py
+++ src/sardana/pool/test/dummyconfs.py
@@ -23,35 +23,23 @@
 ##
 ##############################################################################
 
-__all__ = ['dummyCounterTimerConf01', 'dummyTriggerGateConf01',
-           'dummyMeasurementGroupConf01', 'dummyPoolCTCtrlConf01', 
-           'dummyPoolTGCtrlConf01', 'TangoAttributeCTCtrlConf01']
+__all__ = ['dummyMeasurementGroupConf01',
+           'dummyPoolCTCtrlConf01', 'dummyPoolCTCtrlConf02', 
+           'TangoAttributeCTCtrlConf01', 'TangoAttributeCTCtrlConf02',
+           'dummyPoolTGCtrlConf01', 'dummyPoolTGCtrlConf02',
+           'dummyCounterTimerConf01', 'dummyCounterTimerConf02', 
+           'dummyCounterTimerConf03', 'dummyCounterTimerConf04',
+           'dummyTriggerGateConf01', 'dummyTriggerGateConf02',
+           'dummyTriggerGateConf03', 'dummyTriggerGateConf04']
 
 
-# Pool Elements
-'''Minimum configuration to create a Pool CounterTimer'''
-dummyCounterTimerConf01 = { 'axis': 1,
-                            'ctrl': None,
-                            'full_name': '',
-                            'id': 2,
-                            'name': '',
-                            'pool': None }
-
-'''Minimum configuration to create a Pool TriggerGate'''
-dummyTriggerGateConf01 = { 'axis': 1,
-                            'ctrl': None,
-                            'full_name': '',
-                            'id': 2,
-                            'name': '',
-                            'pool': None }
-
+# Pool Measuremeng Group
 '''Minimum configuration to create a Pool MeasurementGroup'''
 dummyMeasurementGroupConf01 = { 'full_name': '',
                               'id': 3,
                               'name': '',
                               'pool': None,
                               'user_elements': [2] }
-
 
 
 # Pool Ctrls
@@ -66,8 +54,20 @@ dummyPoolCTCtrlConf01 = { 'class_info': None,
                         'pool': None,
                         'properties': {},
                         'role_ids': '',
-                        'type': 'CTExpChannel' }
+                        'type': 'CTExpChannel'}
 
+'''Second minimum configuration to create a Pool CounterTimer controller'''
+dummyPoolCTCtrlConf02 = { 'class_info': None,
+                        'full_name': '',
+                        'id': 2,
+                        'klass': 'DummyCounterTimerController',
+                        'lib_info': None,
+                        'library': 'DummyCounterTimerController.py',
+                        'name': '',
+                        'pool': None,
+                        'properties': {},
+                        'role_ids': '',
+                        'type': 'CTExpChannel'}
 
 '''Minimum configuration to create a TangoAttributeCTcontroller'''
 TangoAttributeCTCtrlConf01 = {'class_info': None,
@@ -76,13 +76,28 @@ TangoAttributeCTCtrlConf01 = {'class_info': None,
                         'id': 1,
                         'klass': 'TangoAttrCTController',
                         'lib_info': None,
-                        'library': 'TangoAttrCTCtrl.py',
+                        #'library': 'TangoAttrCTCtrl.py',
+                        'library': 'TangoAttrCounterTimerController.py',
                         'name': '',
                         'pool': None,
                         'properties': {},
                         'role_ids': '',
                         'type': 'CTExpChannel' }
 
+'''Second minimum configuration to create a TangoAttributeCTcontroller'''
+TangoAttributeCTCtrlConf02 = {'class_info': None,
+                        'ctrl_info': '',
+                        'full_name': '',
+                        'id': 2,
+                        'klass': 'TangoAttrCTController',
+                        'lib_info': None,
+                        #'library': 'TangoAttrCTCtrl.py',
+                        'library': 'TangoAttrCounterTimerController.py',
+                        'name': '',
+                        'pool': None,
+                        'properties': {},
+                        'role_ids': '',
+                        'type': 'CTExpChannel' }
 
 '''Minimum configuration to create a Pool TriggerGate controller'''
 dummyPoolTGCtrlConf01 = { 'class_info': None,
@@ -96,3 +111,83 @@ dummyPoolTGCtrlConf01 = { 'class_info': None,
                         'properties': {},
                         'role_ids': '',
                         'type': 'TGChannel' }
+
+'''Second minimum configuration to create a Pool TriggerGate controller'''
+dummyPoolTGCtrlConf02 = { 'class_info': None,
+                        'full_name': '',
+                        'id': 2,
+                        'klass': 'DummyTriggerGateController',
+                        'lib_info': None,
+                        'library': 'DummyTriggerGateController.py',
+                        'name': '',
+                        'pool': None,
+                        'properties': {},
+                        'role_ids': '',
+                        'type': 'TGChannel' }
+
+
+# Pool Elements
+'''Minimum configuration to create a Pool CounterTimer'''
+dummyCounterTimerConf01 = { 'axis': 1,
+                            'ctrl': None,
+                            'full_name': '',
+                            'id': 2,
+                            'name': '',
+                            'pool': None }
+
+'''Second minimum configuration to create a Pool CounterTimer'''
+dummyCounterTimerConf02 = { 'axis': 2,
+                            'ctrl': None,
+                            'full_name': '',
+                            'id': 3,
+                            'name': '',
+                            'pool': None }
+
+'''Third minimum configuration to create a Pool CounterTimer'''
+dummyCounterTimerConf03 = { 'axis': 1,
+                            'ctrl': None,
+                            'full_name': '',
+                            'id': 2,
+                            'name': '',
+                            'pool': None }
+
+'''Fourth minimum configuration to create a Pool CounterTimer'''
+dummyCounterTimerConf04 = { 'axis': 2,
+                            'ctrl': None,
+                            'full_name': '',
+                            'id': 3,
+                            'name': '',
+                            'pool': None }
+
+'''Minimum configuration to create a Pool TriggerGate'''
+dummyTriggerGateConf01 = { 'axis': 1,
+                            'ctrl': None,
+                            'full_name': '',
+                            'id': 2,
+                            'name': '',
+                            'pool': None }
+
+'''Second minimum configuration to create a Pool TriggerGate'''
+dummyTriggerGateConf02 = { 'axis': 2,
+                            'ctrl': None,
+                            'full_name': '',
+                            'id': 3,
+                            'name': '',
+                            'pool': None }
+
+'''Third minimum configuration to create a Pool TriggerGate'''
+dummyTriggerGateConf03 = { 'axis': 3,
+                            'ctrl': None,
+                            'full_name': '',
+                            'id': 4,
+                            'name': '',
+                            'pool': None }
+
+'''Fourth minimum configuration to create a Pool TriggerGate'''
+dummyTriggerGateConf04 = { 'axis': 4,
+                            'ctrl': None,
+                            'full_name': '',
+                            'id': 5,
+                            'name': '',
+                            'pool': None }
+
--- src/sardana/pool/test/helper.py
+++ src/sardana/pool/test/helper.py
@@ -26,7 +26,8 @@
 __all__ = ['createPoolController', 'createPoolCounterTimer',
            'createPoolTriggerGate', 'createPoolMeasurementGroup',
            'createPoolTGGenerationConfiguration',
-           'createCTAcquisitionConfiguration']
+           'createCTAcquisitionConfiguration', 'createMGConfiguration',
+           'getTGConfiguration']
 
 from sardana.pool.poolcontroller import PoolController
 from sardana.pool.poolcountertimer import PoolCounterTimer
@@ -72,6 +73,7 @@ def createPoolMeasurementGroup(pool, conf):
     kwargs['pool'] = pool
     return PoolMeasurementGroup(**kwargs)
 
+
 def createPoolTGGenerationConfiguration(ctrls, ctrls_conf, 
                                     ctrl_channels, ctrl_channels_conf):
     '''Method to create TGGeneration configuration. Order of the sequences is 
@@ -103,7 +105,6 @@ def createPoolTGGenerationConfiguration(ctrls, ctrls_conf,
         ctrls_configuration[ctrl] = ctrl_conf
     configuration = {'controllers': ctrls_configuration}
     return configuration 
-
 
 
 def createCTAcquisitionConfiguration(ctrls, ctrls_conf, 
@@ -142,3 +143,144 @@ def createCTAcquisitionConfiguration(ctrls, ctrls_conf,
         ctrls_configuration[ctrl] = ctrl_conf
     configuration['controllers'] = ctrls_configuration
     return configuration 
+
+def createMGConfiguration(ctrls, ctrls_conf, ctrl_channels, ctrl_channels_conf,
+                          ctrl_trigger_elements, ctrl_trigger_modes):
+    '''Method to create general MeasurementGroup (and CT) configuration. 
+    Order of the sequences is important. For all sequences, the element of a 
+    given position refers the same controller. 
+    
+    :param ctrls: sequence of the controllers used by the action
+    :type ctrls: seq<sardana.pool.PoolController>
+    :param ctrls_conf: sequence of the controllers configuration dictionaries
+    :type ctrls_conf: dict
+    :param ctrl_channels: sequence of the sequences of the channels 
+    corresponding to the controllers 
+    :type ctrl_channels: seq<seq<sardana.pool.PoolCounterTimer>>
+    :param ctrl_channels_conf: sequence of the sequences of the channels 
+    configuration dictionaries
+    :type ctrl_channels_conf: seq<seq<dict>>
+    :param trigger_elements: sequence of the sequences of the trigger elements
+    :type trigger_elements: seq<seq<sardana.pool.PoolTriggerGate>>
+    :param trigger_modes: sequence of the sequences of the trigger elements
+    :type trigger_modes: seq<seq<str>>
+    :return: a configuration dictionary
+    :rtype: dict<>
+    '''
+
+    _tg_elements = []
+    master_ctrl_idx = 0
+    master_idx = 0
+    MG_configuration = {}
+    MG_configuration['sw_position'] = False
+    MG_configuration['sw_time'] = False
+
+    ctrls_configuration = {}
+    MG_configuration['timer'] = ctrl_channels[master_ctrl_idx][master_idx]
+    for ctrl, ctrl_conf, channels, channels_conf, trigger_elements, \
+            trigger_modes in zip(ctrls, ctrls_conf, ctrl_channels, 
+            ctrl_channels_conf, ctrl_trigger_elements, ctrl_trigger_modes):
+        ctrl_conf['units'] = {}
+        ctrl_conf['units']['0'] = main_unit_data = {}
+        ctrl_conf['units']['0']['channels'] = {}
+        for channel, channel_conf, trigger_element, trigger_mode in \
+              zip(channels, channels_conf, trigger_elements, trigger_modes):
+            main_unit_data['channels'][channel] = channel_conf
+            main_unit_data['channels'][channel]['trigger_element'] = \
+                                                                trigger_element
+            main_unit_data['channels'][channel]['trigger_mode'] = trigger_mode
+            if trigger_element not in _tg_elements:
+                _tg_elements.append(trigger_element)            
+
+        main_unit_data['timer'] = channels[master_idx]
+        ctrls_configuration[ctrl] = ctrl_conf
+    MG_configuration['controllers'] = ctrls_configuration
+
+    if 'sw_position' in _tg_elements:
+        MG_configuration['sw_position'] = True
+    if 'sw_time' in _tg_elements:
+        MG_configuration['sw_time'] = True    
+
+    return MG_configuration
+
+
+def getTGConfiguration(MGcfg):
+    '''Build TG configuration from complete MG configuration.
+
+    :param MGcfg: configuration dictionary of the whole Measurement Group.
+    :type MGcfg: dict<>
+    :return: a configuration dictionary of TG elements organized by controller
+    :rtype: dict<>
+    '''
+
+    # Create list with not repeated elements
+    _tg_element_list = []
+
+    for ctrl in MGcfg["controllers"]:
+        channels_dict = MGcfg["controllers"][ctrl]['units']['0']['channels']
+        for channel in channels_dict:
+            tg_element = channels_dict[channel]['trigger_element']
+            if tg_element not in _tg_element_list:
+                _tg_element_list.append(tg_element)
+
+    # Intermediate dictionary to organize each ctrl with its elements.
+    ctrl_tgelem_dict = {}
+    for tgelem in _tg_element_list:
+        tg_ctrl = tgelem.get_controller()
+        if tg_ctrl not in ctrl_tgelem_dict.keys():
+            ctrl_tgelem_dict[tg_ctrl] = [tgelem]
+        else:
+            ctrl_tgelem_dict[tg_ctrl].append(tgelem)
+
+    # Build TG configuration dictionary.
+    TGcfg = {}
+    TGcfg['controllers'] = {}
+
+    for ctrl in ctrl_tgelem_dict:
+        TGcfg['controllers'][ctrl] = {}
+        TGcfg['controllers'][ctrl]['units'] = {}
+        TGcfg['controllers'][ctrl]['units']['0'] = {}
+        TGcfg['controllers'][ctrl]['units']['0']['channels'] = {}
+        unit = TGcfg['controllers'][ctrl]['units']['0']
+        for tg_elem in ctrl_tgelem_dict[ctrl]:
+            ch = unit['channels'][tg_elem] = {}
+            ch['full_name']= tg_elem.full_name
+
+    return TGcfg
+
+
+
+
+"""
+def walk(dictionary, foundkey, answer=None, sofar=None):
+    if sofar is None:
+        sofar = []
+    if answer is None:
+        answer = []
+    for k,v in dictionary.iteritems():
+        if k == foundkey:
+            answer.append(sofar + [k])
+        if isinstance(v, dict):
+            walk(v, foundkey, answer, sofar+[k])
+    return answer
+
+def delKeys(dictionary, removekey):
+    for path in walk(dictionary, removekey):
+        dd = dictionary
+        while len(path) > 1:
+            dd = dd[path[0]]
+            path.pop(0)
+        dd.pop(path[0])
+
+
+def getCTConfiguration(MGcfg):
+    Extract CT configuration from complete MG configuration.
+
+    CTcfg = copy.deepcopy(MGcfg)
+    delKeys(CTcfg, 'trigger_element')
+    delKeys(CTcfg, 'trigger_mode')
+    #if 'sw_position' in CTcfg: del CTcfg['sw_position']
+    #if 'sw_time' in CTcfg: del CTcfg['sw_time']
+    return CTcfg
+"""
+
