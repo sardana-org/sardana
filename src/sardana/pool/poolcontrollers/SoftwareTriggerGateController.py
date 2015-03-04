114e41ec283e13d37fa4c83783a11c2c9f0fb7db
zreszela <zreszela@cells.es>
2015-03-04 16:10:16 +0100

    Add SoftwareTriggerGateController
    SoftwareTriggerGateController is used for triggering software synchronized
    channels.

---
+++ src/sardana/pool/poolcontrollers/SoftwareTriggerGateController.py
@@ -0,0 +1,221 @@
+##############################################################################
+##
+## This file is part of Sardana
+##
+## http://www.tango-controls.org/static/sardana/latest/doc/html/index.html
+##
+## Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+## Sardana is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Sardana is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with Sardana.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
+
+import time
+import threading
+
+from sardana import State
+from sardana.pool.controller import TriggerGateController
+from sardana.sardanaevent import EventGenerator
+from sardana.pool.pooltriggergate import TGEventType
+
+class RectangularFunctionGenerator(EventGenerator):
+    
+    id = 0
+    
+    def __init__(self, *args, **kwargs):
+        EventGenerator.__init__(self)
+        # function characteristics
+        self._repetitions = 0
+        self._offset = 0
+        self._active_period = 0
+        self._passive_period = 0
+        # threading private members
+        self.__lock = threading.Lock()
+        self.__work = False
+        self.__alive = False
+
+        thread_name = 'RectangularFunctionGenerator-%d' % (self.id)
+        self.__thread = threading.Thread(target=self.__run, name = thread_name)
+        
+    def setRepetitions(self, repetitions):
+        self._repetitions = repetitions
+        
+    def getRepetitions(self):
+        return self._repetitions
+    
+    def setOffset(self, offset):
+        self._offset = offset
+        
+    def getOffset(self):
+        return self._offset
+    
+    def setActivePeriod(self, active_period):
+        self._active_period = active_period
+        
+    def getActivePeriod(self):
+        return self._active_period
+    
+    def setPassivePeriod(self, passive_period):
+        self._passive_period = passive_period
+        
+    def getPassivePeriod(self):
+        return self._passive_period
+    
+    def isGenerating(self):
+        return self.__alive
+    
+    def start(self):
+        '''Start function generator'''
+        self.__lock.acquire()
+        try:
+            if not self.__alive:                                
+                self.__alive = True
+                self.__work = True
+                self.__thread.start()
+        finally:
+            self.__lock.release()
+            
+    def stop(self):
+        '''Stop function generator'''
+        self.__lock.acquire()
+        self.__work = False
+        self.__lock.release()
+    
+    def __run(self):
+        '''Generates Sardana events at requested times'''
+        #TODO: now the generation can be interrupted only at the end of a cycle
+        # allow breakage between the active/passive periods or event during 
+        # sleeping
+        i = 0
+        time.sleep(self._offset)
+        while i < self._repetitions and self.__work:
+            self.fire_event(TGEventType.Active, i)
+            time.sleep(self._active_period)            
+            self.fire_event(TGEventType.Passive, i)
+            time.sleep(self._passive_period)
+            i += 1
+#         self.fire_event(TGEventType.Active, i)
+        self.__alive = False
+        
+
+class SoftwareTriggerGateController(TriggerGateController):
+    """Basic controller intended for demonstration purposes only.
+    """
+    gender = "Simulation"
+    organization = "ALBA-Cells"
+    MaxDevice = 1
+    
+    def __init__(self, inst, props, *args, **kwargs):
+        """Constructor"""
+        TriggerGateController.__init__(self, inst, props, *args, **kwargs)
+        self.tg = {}
+        
+    def add_listener(self, listener):
+        '''Backdoor method to attach listeners. It will be removed whenever 
+        a proper EventChannel mechanism will be implemented'''
+        self.tg[0].add_listener(listener)
+        
+    def SetAxisPar(self, axis, name, value):
+        idx = axis - 1
+        tg = self.tg[idx]
+        name = name.lower()
+        if name == 'offset':
+            tg.setOffset(value)
+        elif name == 'active_period':
+            tg.setActivePeriod(value)
+        elif name == 'passive_period':
+            tg.setPassivePeriod(value)
+        elif name == 'repetitions':
+            tg.setRepetitions(value)
+            
+    def GetAxisPar(self, axis, name):
+        idx = axis - 1
+        tg = self.tg[idx]
+        name = name.lower()
+        if name == 'offset':
+            v = tg.setOffset()
+        elif name == 'active_period':
+            v = tg.setActivePeriod()
+        elif name == 'passive_period':
+            v = tg.getPassivePeriod()
+        elif name == "repetitions":
+            v = tg.getRepetitions()
+        return v
+
+    def AddDevice(self, axis):
+        self._log.debug('AddDevice(%d): entering...' % axis)
+        idx = axis - 1
+        self.tg[idx] = RectangularFunctionGenerator()
+
+    def DeleteDevice(self, axis):
+        pass
+
+    def PreStateAll(self):
+        pass
+
+    def StateAll(self):
+        pass
+
+    def PreStateOne(self, axis):
+        pass
+
+    def StateOne(self, axis):
+        """Get the dummy trigger/gate state"""
+        self._log.debug('StateOne(%d): entering...' % axis)
+        sta = State.On
+        status = "Stopped"
+        idx = axis - 1
+        if self.tg[idx].isGenerating():
+            sta = State.Moving
+            status = "Moving"
+        self._log.debug('StateOne(%d): returning (%s, %s)' % (axis, sta, status))
+        return sta, status
+
+    def PreStartAll(self):
+        pass
+
+    def StartAll(self):
+        pass
+
+    def PreStartOne(self, axis, value=None):
+        return True
+
+    def StartOne(self, axis):
+        """Start the specified trigger
+        """
+        self._log.debug('StartOne(%d): entering...' % axis)
+        idx = axis - 1
+        self.tg[idx].start()        
+
+    def AbortOne(self, axis):
+        """Start the specified trigger
+        """
+        self._log.debug('StartOne(%d): entering...' % axis)
+        idx = axis - 1
+        self.tg[idx].stop()
+
+    def PreReadAll(self):
+        pass
+
+    def ReadAll(self):
+        pass
+
+    def PreReadOne(self,axis):
+        pass
+
+    def ReadOne(self, axis):
+        pass
+
+    def LoadOne(self, axis, value):
+        pass
