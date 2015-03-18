124879618baaabeeb525dce1f3630da5001fc0bd
cfalcon <cfalcon@cells.es>
2015-03-18 19:59:39 +0100

    Refactor the test_recorddata
    ScanDataTestCase class uses now the DummyEventSource to inject the data.
    Modify the asserts to check the dimmention and contents of the nexus file
    comparing them with the input  data.

--- src/sardana/macroserver/scan/test/test_recorddata.py
+++ src/sardana/macroserver/scan/test/test_recorddata.py
@@ -24,14 +24,28 @@
 ##############################################################################
 
 import nxs
+import math
+import numpy
 from taurus.external import unittest
 from sardana.macroserver.scan.scandata import ScanData
 from sardana.macroserver.scan.recorder import DataHandler
 from sardana.macroserver.scan.recorder.storage import NXscan_FileRecorder
-from sardana.macroserver.scan.test.helper import createScanDataEnvironment
-from copy import deepcopy
+from sardana.macroserver.scan.test.helper import (createScanDataEnvironment,
+                                                               DummyEventSource)
+from taurus.test import insertTest
 
-class RecordDataTestCase(unittest.TestCase):
+data1 = [
+            {'label':'ch1', 'data':[10.0, 6.0, 3.4], 'index':[1, 2, 3]},
+            {'label':'ch1', 'data':[10.0, 6.0, 3.4], 'index':[6, 7, 8]}
+]
+data2 = [
+            {'label':'ch2', 'data':[9.2, 7.4], 'index':[1, 3]},
+            {'label':'ch2', 'data':[1.1], 'index':[2]},
+            {'label':'ch2', 'data':[10.0, 3.4], 'index':[4, 7]}
+]
+
+@insertTest(helper_name='recorddata', ldata=[data1, data2])
+class ScanDataTestCase(unittest.TestCase):
     """Use ScanData, DataHandler and ScanDataEnvironment in order to record
     data and verify that the stored data in the NeXus file corresponds with
     the initial data that has been sent for storage.
@@ -41,69 +55,87 @@ class RecordDataTestCase(unittest.TestCase):
         """SetUp
         """
         unittest.TestCase.setUp(self)
-
-        DH = DataHandler()
+        self.DH = DataHandler()
         self.file_name = "/tmp/data_nxs.hdf5"
         NXrecorder = NXscan_FileRecorder(filename=self.file_name, 
                                              macro="dscan", overwrite=True)
-        DH.addRecorder(NXrecorder)
+        self.DH.addRecorder(NXrecorder)
 
-        columns = ['ch1', 'ch2']
+    def prepareScandData(self, ldata):
         ScanDir = '/tmp/'
         ScanFile = 'data_nxs.hdf5'
-        env = createScanDataEnvironment(columns, ScanDir, ScanFile)
+        self.input_chns = [d[0]['label'] for d in ldata]
+        env = createScanDataEnvironment(self.input_chns, ScanDir, ScanFile)
+        self.scanData = ScanData(environment=env, data_handler=self.DH)
 
-        testScanData = ScanData(environment=env, data_handler=DH)
-        testScanData.start()
+        self.srcs = []
+        self.inputs = []
+        self.max_index = -1
+        for data in ldata:
+            name = '_s_%s_' % d[0]['label']
+            des = DummyEventSource(name, self.scanData, data)
+            self.srcs.append(des)
+            # Prepare the input
+            data_input = dict(label='', data=[], index=[])
+            for chuck in data:
+                data_input['label'] = chuck['label']
+                data_input['data'] += chuck['data']
+                data_input['index'] += chuck['index']
+                _max = max(data_input['index'])
+                if self.max_index < _max:
+                    self.max_index = _max
+            self.inputs.append(data_input)
+        # generate the final table
+        self.table = []
+        for input in self.inputs:
+            v = numpy.array([numpy.nan]*self.max_index)
+            for i in range(len(input['index'])):
+                index = input['index'][i] - 1
+                data = input['data'][i]
+                v[index] = data
+            self.table.append(v)
 
-        data1_initial = {'label':'ch1', 'data':[10.0, 6.0, 3.4]}
-        data2_initial = {'label':'ch2', 'data':[9.2, 7.4]}
-        data3_initial = {'label':'ch2', 'data':[1.1]}
-
-        testScanData.addData(data1_initial)
-        testScanData.addData(data2_initial)
-        testScanData.addData(data3_initial)
-
-        testScanData.end()
-
-        data_ch1_input = deepcopy(data1_initial)
-        data_ch2_input = deepcopy(data2_initial)
-        data_ch2_input['data'] = data2_initial['data'] + data3_initial['data']
-        self.inputs = [data_ch1_input, data_ch2_input]
-          
-    def test_recorddata(self):
+    def recorddata(self, ldata):
         """Verify that the data sent for storage is equal 
            to the actual data present in the created NeXus file.
         """
-
+        self.prepareScandData(ldata)
+        # Fill the recoder
+        self.scanData.start()
+        for s in self.srcs:
+            s.start()
+        for s in self.srcs:
+            s.join()
+        self.scanData.end()
+        # Test the generated nxs file
         nxsfile = nxs.open(self.file_name,'r')
         nxsfile.opengroup('entry1')    
         nxsfile.opengroup('measurement')
-
-        lengths_data = []
-        for i in range(len(self.inputs)):
-            lengths_data.append(len(self.inputs[i]['data']))
-        min_inputdata_length = min(lengths_data)
-
-        c = 0
+        chn = 0
+        chn_names = []
         for entry in nxsfile.getentries():
-            if (entry!="pre_scan_snapshot" and entry!="Pt_No"):
-                input_data = self.inputs[c]['data']
-                print(input_data)
+            if (entry != "pre_scan_snapshot" and entry != "Pt_No"):
+                inputdata = self.table[chn]
                 nxsfile.opendata(entry)
                 datastored = nxsfile.getdata()
                 datastored_length = len(datastored)
-                msg = ("For %s: data sent length is not " 
-                       "equal to stored data length" % entry)
+                msg = ("For %s: the input data length is not "
+                                          "equal to stored data length" % entry)
+                self.assertEqual(datastored_length, self.max_index, msg)
 
-                self.assertEqual(datastored_length, min_inputdata_length, msg)
-
-                for i in range(min_inputdata_length):
-                    self.assertEqual(input_data[i], datastored[i], msg)
-                
+                for i in range(len(datastored)):
+                    msg = ('%s: input data is not equal to stored data. '
+                                            'Expected: %s , Read: %s' %\
+                                                 (entry, inputdata, datastored))
+                    self.assertEqual(inputdata[i], datastored[i], msg)
                 nxsfile.closedata()
-                c = c+1
+                chn += 1
+                chn_names.append(entry)
 
+        msg = ('The number of read channels is not equal to input channels '
+                                   'Input channels: %s, Read channels %s' %\
+                                                   (self.input_chns, chn_names))
+        self.assertEqual(chn, len(self.input_chns), msg)
         nxsfile.closegroup()
         nxsfile.closegroup()
         nxsfile.close()
