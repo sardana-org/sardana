a2fd67a824cf0e26b1e452edf113238008cc151f
cfalcon <cfalcon@cells.es>
2015-08-10 16:16:20 +0200

    Add SoftwareTriggerGatePositionController
    Develop a SoftwareTriggerGatePositionController base on
    SoftwareTriggerGateController. This controller add
    two new extra parameters: initial_pos and sign.
    
    The initial_pos is the current position of the moveable.
    The sign indicates the direction of the movement could be 1 or -1
    
    The main logic of the controller is implemented in
    PositionFunctionGenerator.

---
+++ src/sardana/pool/poolcontrollers/SoftwareTriggerGatePositionController.py
@@ -0,0 +1,158 @@
+##############################################################################
+##
+## This file is part of Sardana
+##
+## http://www.tango-controls.org/static/sardana/latest/doc/html/index.html
+##
+## Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+## Sardana is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Sardana is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with Sardana.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
+from sardana import State
+from sardana.util.funcgenerator import PositionFunctionGenerator
+from sardana.pool.controller import TriggerGateController
+
+class SoftwareTriggerGatePositionController(TriggerGateController):
+    """Basic controller intended for demonstration purposes only.
+    """
+    gender = "Simulation"
+    organization = "ALBA-Cells"
+    MaxDevice = 1
+
+    def __init__(self, inst, props, *args, **kwargs):
+        """Constructor"""
+        TriggerGateController.__init__(self, inst, props, *args, **kwargs)
+        self.tg = {}
+
+    def add_listener(self, listener):
+        '''Backdoor method to attach listeners. It will be removed whenever
+        a proper EventChannel mechanism will be implemented'''
+        self.tg[0].add_listener(listener)
+
+    def remove_listener(self, listener):
+        self.tg[0].remove_listener(listener)
+
+    def SetAxisPar(self, axis, name, value):
+        idx = axis - 1
+        tg = self.tg[idx]
+        name = name.lower()
+        if name == 'offset':
+            tg.setOffset(value)
+        elif name == 'active_interval':
+            tg.setActiveInterval(value)
+        elif name == 'passive_interval':
+            tg.setPassiveInterval(value)
+        elif name == 'repetitions':
+            tg.setRepetitions(value)
+        elif name == 'initial_pos':
+            tg.setInitialPos(value)
+        elif name == 'sign':
+            tg.setSign(value)
+
+    def GetAxisPar(self, axis, name):
+        idx = axis - 1
+        tg = self.tg[idx]
+        name = name.lower()
+        if name == 'offset':
+            v = tg.getOffset()
+        elif name == 'active_interval':
+            v = tg.getActiveInterval()
+        elif name == 'passive_interval':
+            v = tg.getPassiveInterval()
+        elif name == "repetitions":
+            v = tg.getRepetitions()
+        elif name == "initial_pos":
+            v = tg.getInitialPos()
+        elif name == "sign":
+            v = tg.getSign()
+        else:
+            v = None
+            msg = 'GetAxisPar(%d): has not attribute %s' %(axis, name)
+            self._log.debug(msg)
+        return v
+
+    def AddDevice(self, axis):
+        self._log.debug('AddDevice(%d): entering...' % axis)
+        idx = axis - 1
+        self.tg[idx] = PositionFunctionGenerator()
+
+    def GetDevice(self, axis):
+        idx = axis - 1
+        return self.tg[idx]
+
+    def DeleteDevice(self, axis):
+        pass
+
+    def PreStateAll(self):
+        pass
+
+    def StateAll(self):
+        pass
+
+    def PreStateOne(self, axis):
+        pass
+
+    def StateOne(self, axis):
+        """Get the dummy trigger/gate state"""
+        self._log.debug('StateOne(%d): entering...' % axis)
+        sta = State.On
+        status = "Stopped"
+        idx = axis - 1
+        if self.tg[idx].isGenerating():
+            sta = State.Moving
+            status = "Moving"
+        self._log.debug('StateOne(%d): returning (%s, %s)' % (axis, sta, status))
+        return sta, status
+
+    def PreStartAll(self):
+        pass
+
+    def StartAll(self):
+        pass
+
+    def PreStartOne(self, axis, value=None):
+        self._log.debug('PreStartOne(%d): entering...' % axis)
+        idx = axis - 1
+        self.tg[idx].prepare()
+        return True
+
+    def StartOne(self, axis):
+        """Start the specified trigger
+        """
+        self._log.debug('StartOne(%d): entering...' % axis)
+        idx = axis - 1
+        self.tg[idx].start()
+
+    def AbortOne(self, axis):
+        """Start the specified trigger
+        """
+        self._log.debug('AbortOne(%d): entering...' % axis)
+        idx = axis - 1
+        self.tg[idx].stop()
+
+    def PreReadAll(self):
+        pass
+
+    def ReadAll(self):
+        pass
+
+    def PreReadOne(self,axis):
+        pass
+
+    def ReadOne(self, axis):
+        pass
+
+    def LoadOne(self, axis, value):
+        pass
