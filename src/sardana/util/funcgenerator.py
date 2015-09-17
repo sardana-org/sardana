d3a169b0f6c61563fba6d054ab389217178d470d
zreszela <zreszela@cells.es>
2015-09-17 18:53:41 +0200

    Use "list of positions" logic in funtion generator
    Refactor PositionFunctionGenerator in order to use list of positions (and their
    corresponding event ids, eventy types and event condition functions) instead
    of dynamically generating next targets while the generation advances.

--- src/sardana/util/funcgenerator.py
+++ src/sardana/util/funcgenerator.py
@@ -1,6 +1,7 @@
 import time
 import threading
 import math
+import numpy as np
 from sardana.sardanaevent import EventGenerator
 from sardana.pool.pooltriggergate import TGEventType
 
@@ -141,13 +142,11 @@ class PositionFunctionGenerator(EventGenerator):
         EventGenerator.__init__(self)
         # function characteristics
         self._event = threading.Event()
-        self._intervals = []
-        self._sign = 1
-        self._initial_pos = 0
-        self._repetitions = 0
-        self._offset = 0
-        self._active_interval = 0
-        self._passive_interval = 0
+        self.event_values = []
+        self.event_ids = []
+        self.event_types = []
+        self.event_conditions = []
+        self.last_value = None
         # threading private members
         self.__lock = threading.Lock()
         self.__thread = None # will be allocated in prepare
@@ -161,18 +160,9 @@ class PositionFunctionGenerator(EventGenerator):
 
     def event_received(self, *args, **kwargs):
         _, t, v = args
-        if self._target is not None:
-            # Evaluate a target
-            if self.isStep:
-                if v >= self._target:
-                    self._event.set()
-                    self._target += self._step
-                    self.isStep = False
-            # Evaluate if the active_interval has expired
-            elif v >= self._target - self._passive_interval * self._sign:
-                self._event.set()
-                self.isStep = True
-
+        print v.value
+        self.last_value = v.value
+        self._event.set()
 
     def calc_step(self):
         step = (self._active_interval + self._passive_interval) * self._sign
@@ -214,6 +204,13 @@ class PositionFunctionGenerator(EventGenerator):
     def getPassiveInterval(self):
         return self._passive_interval
 
+    def setConfiguration(self, values, conditions, types, ids):
+        assert len(values) == len(conditions) == len(types) == len(ids)
+        self.event_values = values
+        self.event_types = types
+        self.event_ids = ids
+        self.event_conditions = conditions
+
     def partialSleep(self, necessary_naps, nap_time):
         '''Performs externally stoppable sleep
         '''
@@ -251,23 +248,36 @@ class PositionFunctionGenerator(EventGenerator):
 
     def __run(self):
         '''Generates Sardana events at requested times'''
-        i = 0
-        self.calc_step()
-        self._target = self._initial_pos + self._offset + self._step
         try:
-            while i < self._repetitions:
+            while len(self.event_values) > 1:
                 with self.__lock:
                     if self.__stop:
                         raise StopException('Function generation stopped')
                 self._event.wait()
-                self.fire_event(TGEventType.Active, i)
-                # TODO: Study a better way to do it.
-                # Maybe an hardcoded time.sleep is better.
-                self._event.wait()
-                self.fire_event(TGEventType.Passive, i)
-                i += 1
-                # TODO: Study if a partial sleep for abort is needed
-        except StopException, e:
+                # reset flag so in next iteration we will wait for a new update
+                self._event.clear()
+                idx = 0
+                candidate = self.event_values[idx]
+                condition = self.event_conditions[idx]
+                if condition(self.last_value, candidate):
+                    # checking if we need to skip some events
+                    while len(self.event_values) > idx + 2:
+                        candidate = self.event_values[idx + 1]
+                        condition = self.event_conditions[idx + 1]
+                        if condition(candidate, self.last_value):
+                            break
+                        idx += 1
+                    # emit the corresponding event
+                    event_id = self.event_ids[idx]
+                    event_type = self.event_types[idx]
+                    print "Candidate: %f; Update: %f" % (self.event_values[idx], self.last_value)
+                    self.fire_event(event_type, event_id)
+                    # eliminate sent and lost events from the list 
+                    self.event_values = self.event_values[idx+1:]
+                    self.event_conditions = self.event_conditions[idx+1:]
+                    self.event_types = self.event_types[idx+1:]
+                    self.event_ids = self.event_ids[idx+1:]
+        except StopException:
             pass
         finally:
             self.__alive = False
