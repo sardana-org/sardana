sardana-org/sardana

commit ba250d470c4c76228d4fc79d52e35af128ad1d80

Refactor to use SynchParam enumeration

src/sardana/sardanacustomsettings.py
@@ -31,7 +31,7 @@ aspects of Sardana.
 
 #:UnitTest door name: the door to be used by unit tests.
 #:UNITTEST_DOOR_NAME Must be defined for running sardana unittests. 
-UNITTEST_DOOR_NAME = "door/demo1/1"
+UNITTEST_DOOR_NAME = "door/rhoms/1"
 #:UnitTests Pool DS name: Pool DS to use in unit tests.
 UNITTEST_POOL_DS_NAME = "unittest1"
 #:UnitTests Pool Device name: Pool Device to use in unit tests.

src/sardana/macroserver/scan/gscan.py
@@ -49,7 +49,7 @@ from taurus.core.tango import FROM_TANGO_TO_STR_TYPE
 from sardana.util.tree import BranchNode, LeafNode, Tree
 from sardana.util.motion import Motor as VMotor
 from sardana.util.motion import MotionPath
-from sardana.pool.pooldefs import SynchDomain
+from sardana.pool.pooldefs import SynchDomain, SynchParam
 from sardana.macroserver.msexception import MacroServerException, UnknownEnv, \
     InterruptException, StopException
 from sardana.macroserver.msparameter import Type
@@ -1863,13 +1863,13 @@ class CTScan(CScan):
             initial_position = start
             total_time = total_position / path.max_vel
             delay_time = path.max_vel_time
-            synchronization = [{'delay': {SynchDomain.Time:delay_time},
-                                'initial': {SynchDomain.Position:initial_position},
-                                'active': {SynchDomain.Position:active_position,
+            synchronization = [{SynchParam.Delay: {SynchDomain.Time:delay_time},
+                                SynchParam.Initial: {SynchDomain.Position:initial_position},
+                                SynchParam.Active: {SynchDomain.Position:active_position,
                                           SynchDomain.Time:active_time},
-                                'total': {SynchDomain.Position:total_position,
+                                SynchParam.Total: {SynchDomain.Position:total_position,
                                          SynchDomain.Time:total_time},
-                                'repeats': repeats}]
+                                SynchParam.Repeats: repeats}]
             self.debug('Synchronization: %s' % synchronization)
             self.measurement_group.setSynchronization(synchronization)
             self.macro.checkPoint()

src/sardana/pool/poolcontrollers/DummyTriggerGateController.py
@@ -23,7 +23,7 @@
 
 
 from sardana import State
-from sardana.pool.pooldefs import SynchDomain
+from sardana.pool.pooldefs import SynchDomain, SynchParam
 from sardana.util.funcgenerator import RectangularFunctionGenerator
 from sardana.pool.controller import TriggerGateController
 
@@ -38,6 +38,7 @@ class DummyTriggerGateController(TriggerGateController):
         """Constructor"""
         TriggerGateController.__init__(self, inst, props, *args, **kwargs)
         self.tg = {}
+        self.conf = None
 
 #     def add_listener(self, listener):
 #         '''Backdoor method to attach listeners. It will be removed whenever 
@@ -49,11 +50,11 @@ class DummyTriggerGateController(TriggerGateController):
         tg = self.tg[idx]
         # TODO: implement nonequidistant triggering
         conf = conf[0]
-        delay = conf['delay'][SynchDomain.Time]
-        total_time = conf['total'][SynchDomain.Time]
-        active_time = conf['active'][SynchDomain.Time]
+        delay = conf[SynchParam.Delay][SynchDomain.Time]
+        total_time = conf[SynchParam.Total][SynchDomain.Time]
+        active_time = conf[SynchParam.Active][SynchDomain.Time]
         passive_time = total_time - active_time
-        repeats = conf['repeats']
+        repeats = conf[SynchParam.Repeats]
         tg.setOffset(delay)
         tg.setActiveInterval(active_time)
         tg.setPassiveInterval(passive_time)
@@ -63,15 +64,14 @@ class DummyTriggerGateController(TriggerGateController):
         idx = axis - 1
         tg = self.tg[idx]
         # TODO: implement nonequidistant triggering
+        # TODO: wrong configuration dict. return the same conf.
         active_time=tg.getActiveInterval(),
         passive_time=tg.getPassiveInterval()
         total_time = active_time + passive_time
-        conf = list(dict(delay=tg.getOffset(),
-                         total=total_time,
-                         active=active_time,
-                         repeats=tg.getRepetitions()
-                         )
-                    )
+        conf = [{SynchParam.Delay: tg.getOffset(),
+                 SynchParam.Total: total_time,
+                 SynchParam.Active: active_time,
+                 SynchParam.Repeats: tg.getRepetitions()}]
         return conf
 
     def SetAxisPar(self, axis, name, value):

src/sardana/pool/poolcontrollers/SoftwareTriggerGateController.py
@@ -21,7 +21,7 @@
 ##
 ##############################################################################
 from sardana import State
-from sardana.pool.pooldefs import SynchDomain
+from sardana.pool.pooldefs import SynchDomain, SynchParam
 from sardana.util.funcgenerator import RectangularFunctionGenerator
 from sardana.pool.controller import TriggerGateController
 
@@ -60,11 +60,11 @@ class SoftwareTriggerGateController(TriggerGateController):
         tg = self.tg[idx]
         # TODO: implement nonequidistant triggering
         conf = conf[0]
-        delay = conf['delay'][SynchDomain.Time]
-        total_time = conf['total'][SynchDomain.Time]
-        active_time = conf['active'][SynchDomain.Time]
+        delay = conf[SynchParam.Delay][SynchDomain.Time]
+        total_time = conf[SynchParam.Total][SynchDomain.Time]
+        active_time = conf[SynchParam.Active][SynchDomain.Time]
         passive_time = total_time - active_time
-        repeats = conf['repeats']
+        repeats = conf[SynchParam.Repeats]
         tg.setOffset(delay)
         tg.setActiveInterval(active_time)
         tg.setPassiveInterval(passive_time)
@@ -74,14 +74,14 @@ class SoftwareTriggerGateController(TriggerGateController):
         idx = axis - 1
         tg = self.tg[idx]
         # TODO: implement nonequidistant triggering
+        # TODO: wrong configuration dict. return the same conf.
         active_time=tg.getActiveInterval(),
         passive_time=tg.getPassiveInterval()
         total_time = active_time + passive_time
-        conf = [dict(delay=tg.getOffset(),
-                         total=total_time,
-                         active=active_time,
-                         repeats=tg.getRepetitions()
-                         )]
+        conf = [{SynchParam.Delay: tg.getOffset(),
+                 SynchParam.Total: total_time,
+                 SynchParam.Active: active_time,
+                 SynchParam.Repeats: tg.getRepetitions()}]
         return conf
 
     def AddDevice(self, axis):

src/sardana/pool/poolcontrollers/SoftwareTriggerGatePositionController.py
@@ -22,7 +22,7 @@
 ##############################################################################
 import numpy as np
 from sardana import State
-from sardana.pool.pooldefs import SynchDomain
+from sardana.pool.pooldefs import SynchDomain, SynchParam
 from sardana.util.funcgenerator import PositionFunctionGenerator
 from sardana.pool.controller import TriggerGateController
 from sardana.pool.pooltriggergate import TGEventType
@@ -77,12 +77,12 @@ class SoftwareTriggerGatePositionController(TriggerGateController):
         event_ids = []
         event_id = 0
         for group in configuration:
-            repeats = group['repeats']
-            initial = group['initial'][SynchDomain.Position]
+            repeats = group[SynchParam.Repeats]
+            initial = group[SynchParam.Initial][SynchDomain.Position]
             for repeat in xrange(repeats):
                 event_values.append(initial)
                 event_types.append(TGEventType.Active)
-                active = group['active'][SynchDomain.Position]
+                active = group[SynchParam.Active][SynchDomain.Position]
                 # determine the event conditions
                 comparison = np.greater_equal
                 if active < 0:
@@ -92,7 +92,7 @@ class SoftwareTriggerGatePositionController(TriggerGateController):
                 event_values.append(final)
                 event_types.append(TGEventType.Passive)
                 event_ids.extend([event_id, event_id])
-                total = group['total'][SynchDomain.Position]
+                total = group[SynchParam.Total][SynchDomain.Position]
                 initial = initial + total
                 event_id += 1
                 repeat += 1

src/sardana/pool/poolcontrollers/test/test_DummyTriggerGateController.py
@@ -2,23 +2,22 @@ from taurus.test.base import insertTest
 from taurus.external import unittest
 
 from sardana.pool.pooltggeneration import PoolTGGeneration
-from sardana.pool.pooldefs import SynchDomain
+from sardana.pool.pooldefs import SynchDomain, SynchParam
 
 from sardana.pool.test import (FakePool, createPoolController,
                                createPoolTriggerGate, dummyPoolTGCtrlConf01,
                                dummyTriggerGateConf01, 
                                createPoolTGGenerationConfiguration)
 
-synchronization1 = [dict(delay={SynchDomain.Time: 0},
-                         active={SynchDomain.Time: .03},
-                         total={SynchDomain.Time: .1},
-                         repeats=0)
-                    ]
-synchronization2 = [dict(delay={SynchDomain.Time: 0},
-                         active={SynchDomain.Time: .01},
-                         total={SynchDomain.Time: .02},
-                         repeats=10)
-                    ]
+synchronization1 = [{SynchParam.Delay: {SynchDomain.Time: 0},
+                     SynchParam.Active: {SynchDomain.Time: .03},
+                     SynchParam.Total: {SynchDomain.Time: .1},
+                     SynchParam.Repeats: 0}]
+
+synchronization2 = [{SynchParam.Delay: {SynchDomain.Time: 0},
+                     SynchParam.Active: {SynchDomain.Time: .01},
+                     SynchParam.Total: {SynchDomain.Time: .02},
+                     SynchParam.Repeats: 10}]
 
 @insertTest(helper_name='generation', synchronization=synchronization1)
 @insertTest(helper_name='generation', synchronization=synchronization2)

src/sardana/pool/poolcontrollers/test/test_SoftwareTriggerGateController.py
@@ -27,7 +27,7 @@ from taurus.test import insertTest
 from taurus.external import unittest
 
 from sardana.pool.pooltggeneration import PoolTGGeneration
-from sardana.pool.pooldefs import SynchDomain
+from sardana.pool.pooldefs import SynchDomain, SynchParam
 
 from sardana.pool.test import (FakePool, createPoolController,
                                createPoolTriggerGate, softwarePoolTGCtrlConf01,
@@ -39,26 +39,25 @@ from sardana.pool.poolcontrollers.test import (TriggerGateControllerTestCase,
 from sardana.pool.poolcontrollers.SoftwareTriggerGateController import\
                                                   SoftwareTriggerGateController
 
-synchronization1 = [dict(delay={SynchDomain.Time: 0},
-                         active={SynchDomain.Time: .03},
-                         total={SynchDomain.Time: .1},
-                         repeats=0)
-                    ]
-synchronization2 = [dict(delay={SynchDomain.Time: 0},
-                         active={SynchDomain.Time: .01},
-                         total={SynchDomain.Time: .02},
-                         repeats=10)
-                    ]
-synchronization3 = [dict(delay={SynchDomain.Time: 0},
-                         active={SynchDomain.Time: .1},
-                         total={SynchDomain.Time: .1},
-                         repeats=0)
-                    ]
-synchronization4 = [dict(delay={SynchDomain.Time: 0},
-                         active={SynchDomain.Time: .1},
-                         total={SynchDomain.Time: .15},
-                         repeats=3)
-                    ]
+synchronization1 = [{SynchParam.Delay: {SynchDomain.Time: 0},
+                     SynchParam.Active: {SynchDomain.Time: .03},
+                     SynchParam.Total: {SynchDomain.Time: .1},
+                     SynchParam.Repeats: 0}]
+
+synchronization2 = [{SynchParam.Delay: {SynchDomain.Time: 0},
+                     SynchParam.Active: {SynchDomain.Time: .01},
+                     SynchParam.Total: {SynchDomain.Time: .02},
+                     SynchParam.Repeats: 10}]
+
+synchronization3 = [{SynchParam.Delay: {SynchDomain.Time: 0},
+                     SynchParam.Active: {SynchDomain.Time: .1},
+                     SynchParam.Total: {SynchDomain.Time: .1},
+                     SynchParam.Repeats:0}]
+
+synchronization4 = [{SynchParam.Delay: {SynchDomain.Time: 0},
+                     SynchParam.Active: {SynchDomain.Time: .1},
+                     SynchParam.Total: {SynchDomain.Time: .15},
+                     SynchParam.Repeats: 3}]
 
 @insertTest(helper_name='generation',  configuration=synchronization1)
 @insertTest(helper_name='abort', configuration=synchronization2, abort=.1)
@@ -117,10 +116,12 @@ class PoolSoftwareTriggerGateTestCase(unittest.TestCase):
         args = ()
         # composing synchronization configuration
         total_interval = active_interval + passive_interval
-        synchronization = [dict(delay={SynchDomain.Time: offset},
-                                active={SynchDomain.Time: active_interval},
-                                total={SynchDomain.Time: total_interval},
-                                repeats=repetitions)
+        synchronization = [{SynchParam.Delay: {SynchDomain.Time: offset},
+                            SynchParam.Active: {SynchDomain.Time:
+                                                   active_interval},
+                            SynchParam.Total: {SynchDomain.Time:
+                                                   total_interval},
+                            SynchParam.Repeats: repetitions}
                            ]
         kwargs = {'config': self.cfg,
                   'synchronization': synchronization

src/sardana/pool/poolcontrollers/test/test_SoftwareTriggerGatePositionControllerTestCase.py
@@ -25,26 +25,24 @@
 
 
 from taurus.test import insertTest
-from sardana.pool.pooldefs import SynchDomain
+from sardana.pool.pooldefs import SynchDomain, SynchParam
 from sardana.pool.poolcontrollers.test import (TriggerGateControllerTestCase,
                                                PositionGenerator,
                                                TriggerGateReceiver)
 from sardana.pool.poolcontrollers.SoftwareTriggerGatePositionController import\
                                         SoftwareTriggerGatePositionController
 
-synchronization1 = [dict(delay={SynchDomain.Position: 0},
-                         initial={SynchDomain.Position: 0},
-                         active={SynchDomain.Position: .1},
-                         total={SynchDomain.Position: 1},
-                         repeats=10)
-                    ]
+synchronization1 = [{SynchParam.Delay: {SynchDomain.Position: 0},
+                     SynchParam.Initial: {SynchDomain.Position: 0},
+                     SynchParam.Active: {SynchDomain.Position: .1},
+                     SynchParam.Total: {SynchDomain.Position: 1},
+                     SynchParam.Repeats: 10}]
 
-synchronization2 = [dict(delay={SynchDomain.Position: 0},
-                         initial={SynchDomain.Position: 0},
-                         active={SynchDomain.Position: -1},
-                         total={SynchDomain.Position: -1.1},
-                         repeats=10)
-                    ]
+synchronization2 = [{SynchParam.Delay: {SynchDomain.Position: 0},
+                     SynchParam.Initial: {SynchDomain.Position: 0},
+                     SynchParam.Active: {SynchDomain.Position: -1},
+                     SynchParam.Total: {SynchDomain.Position: -1.1},
+                     SynchParam.Repeats: 10}]
 
 @insertTest(helper_name='generation', configuration=synchronization1)
 @insertTest(helper_name='abort', configuration=synchronization2, abort=0.5)
@@ -75,7 +73,7 @@ class SoftwareTriggerGatePositionControllerTestCase(TriggerGateControllerTestCas
         conf = self.ctrl.GetConfiguration(self.AXIS)
         repetitions = 0
         for group in conf:
-            repetitions += group['repeats']
+            repetitions += group[SynchParam.Repeats]
         msg = ('Received triggers: %d does not correspond to generated: %d' %\
                (received_triggers, repetitions))
         if not self.isAborted:

commit b360d9c4c84191ec5fdde9d2178c4c7dbecfdfe4

Fix sardanacustomsetting broken in ba250d470c

src/sardana/sardanacustomsettings.py
@@ -31,7 +31,7 @@ aspects of Sardana.
 
 #:UnitTest door name: the door to be used by unit tests.
 #:UNITTEST_DOOR_NAME Must be defined for running sardana unittests. 
-UNITTEST_DOOR_NAME = "door/rhoms/1"
+UNITTEST_DOOR_NAME = "door/demo1/1"
 #:UnitTests Pool DS name: Pool DS to use in unit tests.
 UNITTEST_POOL_DS_NAME = "unittest1"
 #:UnitTests Pool Device name: Pool Device to use in unit tests.