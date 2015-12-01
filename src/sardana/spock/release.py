78b65e5e84a428b7493d242a0a3c1d000b2e3623
zreszela <zreszela@cells.es>
2015-12-01 16:30:44 +0100

    Add reminder of major version bump
    Next Sardana release should bump the major version number due to the backwards
    incompatibility of recorder imports.

--- src/sardana/spock/release.py
+++ src/sardana/spock/release.py
@@ -39,6 +39,7 @@ name = 'spock'
 revision = '1'
 
 #version = '0.8.1.svn.r' + revision.rstrip('M')
+# BUMP TO 2.0.0 DUE TO BACKWARDS INCOMPATIBILITY OF FEATURE-380
 version = '1.6.1'
 
 description = "An enhanced interactive Macro Server shell."
