f18b119bbe7ea83cdc19569c6168eda898d93412
cfalcon <cfalcon@cells.es>
2016-07-06 09:05:27 +0200

    Revert "Fix wrong adaptation"
    This reverts commit f58852f1114a3d7ec1ac2ed36a647049f018fe51.

--- src/sardana/spock/ipython_00_10/genutils.py
+++ src/sardana/spock/ipython_00_10/genutils.py
@@ -556,10 +556,10 @@ def _get_dev(dev_type):
         return ret
     
     dev_obj_name = '%s_NAME' % dev_type
-    dev_name = ip.user_ns[dev_obj_name]
-    factory = Factory()
     # TODO: For Taurus 4 compatibility
-    dev_obj = factory.getDevice("tango://%s" % dev_name)
+    dev_name = "tango://%s" % ip.user_ns[dev_obj_name]
+    factory = Factory()
+    dev_obj = factory.getDevice(dev_name)
     ip.user_ns[dev_type] = PyTango.DeviceProxy(dev_name) 
     ip.user_ns["_" + dev_type] = dev_obj
     setattr(ip, '_%s' % dev_type, dev_obj)
