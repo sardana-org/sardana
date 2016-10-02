ea0f82f67d1ceb0a1ae2266b800bffa355bec77a
zreszela <zreszela@cells.es>
2016-10-02 13:23:31 +0200

    Remove unnecessary readout

--- src/sardana/pool/poolacquisition.py
+++ src/sardana/pool/poolacquisition.py
@@ -542,18 +542,12 @@ class PoolAcquisitionHardware(PoolAcquisitionBase):
         i = 0
 
         states, values = {}, {}
-        for element in self._channels:
+        for element in self._channels_read_when_acq:
             states[element] = None
             values[element] = None
 
         nap = self._acq_sleep_time
         nb_states_per_value = self._nb_states_per_value
-
-        # read values to send a first event when starting to acquire
-        with ActionContext(self):
-            self.raw_read_value_loop(ret=values)
-            for acquirable, value in values.items():
-                acquirable.put_value_chunk(value, propagate=2)
 
         while True:
             self.read_state_info(ret=states)
@@ -562,12 +556,18 @@ class PoolAcquisitionHardware(PoolAcquisitionBase):
 
             # read value every n times
             if not i % nb_states_per_value:
-                self.read_value_loop(ret=values)
+                self.read_value_loop(ret=values,
+                                     ctrls=self._pool_ctrls_read_when_acq)
                 for acquirable, value in values.items():
                     acquirable.put_value_chunk(value)
 
             time.sleep(nap)
             i += 1
+
+        states, values = {}, {}
+        for element in self._channels:
+            states[element] = None
+            values[element] = None
 
         with ActionContext(self):
             self.raw_read_state_info(ret=states)
