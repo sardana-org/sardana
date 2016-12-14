34ec2259e95f989c9f76a80aad3564932688f740
teresa <tnunez@mail.desy.de>
2016-12-14 09:38:39 +0100

    Forcing read position from hardware

--- src/sardana/pool/poolcontrollers/HklPseudoMotorController.py
+++ src/sardana/pool/poolcontrollers/HklPseudoMotorController.py
@@ -763,7 +763,7 @@ class DiffracBasis(PseudoMotorController):
         motor_position = []
         for i in range(0, self.nb_ph_axes):
             motor = self.GetMotor(i)
-            motor_position.append(motor.position.value)
+            motor_position.append(motor.get_position(cache=False).value)
         self.geometry.axis_values_set(motor_position, USER)
             
         curr_physical_pos = self.geometry.axis_values_get(USER)
@@ -823,7 +823,7 @@ class DiffracBasis(PseudoMotorController):
         motor_position = []
         for i in range(0, self.nb_ph_axes):
             motor = self.GetMotor(i)
-            motor_position.append(motor.position.value)
+            motor_position.append(motor.get_position(cache=False).value)
         self.geometry.axis_values_set(motor_position, USER)
         newref = self.sample.add_reflection(
             self.geometry, self.detector, value[0], value[1], value[2])
