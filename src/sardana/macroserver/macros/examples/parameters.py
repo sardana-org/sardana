ec36a16f834d1347337ddbea5b344a19b27c7696
teresa <tnunez@mail.desy.de>
2017-01-18 15:30:03 +0100

    Using example usages

--- src/sardana/macroserver/macros/examples/parameters.py
+++ src/sardana/macroserver/macros/examples/parameters.py
@@ -25,11 +25,15 @@
 
 from sardana.macroserver.macro import *
 
-__all__ = ["pt0", "pt1", "pt2", "pt3", "pt4", "pt5", "pt6", "pt7", "pt8",
-           "pt9"]
+__all__ = ["pt0", "pt1", "pt2", "pt3", "pt3d", "pt4", "pt5", "pt6", "pt7",
+           "pt7d1", "pt7d2", "pt8", "pt9", "pt10", "pt11", "pt12", "pt13",
+           "pt14", "pt14d", "twice"]
 
 class pt0(Macro):
-    """Macro without parameters. Pretty dull"""
+    """Macro without parameters. Pretty dull.
+       Usage, ex.:
+       pt0
+       """
 
     param_def = []
     
@@ -39,7 +43,10 @@ class pt0(Macro):
 class pt1(Macro):
     """Macro with one float parameter: Each parameter is described in the 
     param_def sequence as being a sequence of four elements: name, type, 
-    default value and description"""
+    default value and description.
+    Usage, ex.:
+    pt1 1
+    """
     
     param_def = [ [ 'value', Type.Float, None, 'some bloody float'] ]
     
@@ -49,7 +56,10 @@ class pt1(Macro):
 class pt2(Macro):
     """Macro with one Motor parameter: Each parameter is described in the 
     param_def sequence as being a sequence of four elements: name, type, 
-    default value and description"""
+    default value and description.
+    Usage, ex.
+    pt2 mot1
+    """
 
     param_def = [ [ 'motor', Type.Motor, None, 'some bloody motor'] ]
     
@@ -60,10 +70,36 @@ class pt3(Macro):
     """Macro with a list of numbers as parameter: the type is a sequence of
     parameter types which is repeated. In this case it is a repetition of a 
     float so only one parameter is defined.
-    By default the repetition as a semantics of 'at least one'"""
+    By default the repetition as a semantics of 'at least one'
+    Usages, ex.:
+    pt3 [[1][34][15]]
+    pt3 [1 34 15]
+    pt3 1 34 15
+    """
 
     param_def = [
        [ 'numb_list', [ [ 'pos', Type.Float, None, 'value'] ], None, 'List of values'],
+    ]
+    
+    def run(self, *args, **kwargs):
+        pass
+    
+class pt3d(Macro):
+    """Macro with a list of numbers as parameter: the type is a sequence of
+    parameter types which is repeated. In this case it is a repetition of a 
+    float so only one parameter is defined. The parameter has a default value.
+    By default the repetition as a semantics of 'at least one'
+    Usages, ex.:
+    pt3d [[1][34][15]]
+    pt3d [1 34 15]
+    pt3d 1 34 15
+    Usages taken the default value, ex.:
+    pt3d [[1] [] [15]]
+    pt3d [1 [] 15]
+    """
+
+    param_def = [
+       [ 'numb_list', [ [ 'pos', Type.Float, 21, 'value'] ], None, 'List of values'],
     ]
     
     def run(self, *args, **kwargs):
@@ -73,7 +109,12 @@ class pt4(Macro):
     """Macro with a list of motors as parameter: the type is a sequence of
     parameter types which is repeated. In this case it is a repetition of a 
     motor so only one parameter is defined.
-    By default the repetition as a semantics of 'at least one'"""
+    By default the repetition as a semantics of 'at least one'.
+    Usages, ex.:
+    pt3 [[mot1][mot2][mot3]]
+    pt3 [mot1 mot2 mot3]
+    pt3 mot1 mot2 mot3
+    """
 
     param_def = [
        [ 'motor_list', [ [ 'motor', Type.Motor, None, 'motor name'] ], None, 'List of motors'],
@@ -83,7 +124,12 @@ class pt4(Macro):
         pass
 
 class pt5(Macro):
-    """Macro with a motor parameter followed by a list of numbers"""
+    """Macro with a motor parameter followed by a list of numbers.
+    Usages, ex.:
+    pt5 mot1 [[1][3]]
+    pt5 mot1 [1 3]
+    pt5 mot1 1 3
+    """
 
     param_def = [
        [ 'motor', Type.Motor, None, 'Motor to move'],
@@ -92,11 +138,16 @@ class pt5(Macro):
     
     def run(self, *args, **kwargs):
         pass
-
+ 
 class pt6(Macro):
     """Macro with a motor parameter followed by a list of numbers. The list as
     explicitly stated an optional last element which is a dictionary that defines the
-    min and max values for repetitions"""
+    min and max values for repetitions.
+    Usages, ex.:
+    pt6 mot1 [[1][34][1]]
+    pt6 mot1 [1 34 1]
+    pt6 mot1 1 34 1
+    """
 
     param_def = [
        [ 'motor', Type.Motor, None, 'Motor to move'],
@@ -107,7 +158,12 @@ class pt6(Macro):
         pass
     
 class pt7(Macro):
-    """Macro with a list of pair Motor,Float"""
+    """Macro with a list of pair Motor,Float.
+    Usages, ex.:
+    pt7 [[mot1 1][mot2 3]]
+    pt7 [mot1 1 mot2 3]
+    pt7 mot1 1 mot2 3
+    """
 
     param_def = [
        [ 'm_p_pair', [ [ 'motor', Type.Motor, None, 'Motor to move'],
@@ -117,10 +173,57 @@ class pt7(Macro):
     
     def run(self, *args, **kwargs):
         pass
+    
+class pt7d1(Macro):
+    """Macro with a list of pair Motor,Float. Default value for last ParamRepeat element.
+    Usages, ex.:
+    pt7d1 [[mot1 1][mot2 3]]
+    pt7d1 [mot1 1 mot2 3]
+    pt7d1 mot1 1 mot2 3
+    Using default value, ex.:
+    pt7d1 [[mot1][mot2 3]] # at any repetition
+    pt7d1 [mot1 1 [mot2]] # only at the last repetition
+    
+    """
+
+    param_def = [
+       [ 'm_p_pair', [ [ 'motor', Type.Motor, None, 'Motor to move'],
+                       [ 'pos',   Type.Float, 2, 'Position to move to'] ],
+         None, 'List of motor/position pairs']
+    ]
+    
+    def run(self, *args, **kwargs):
+        pass
+
+    
+class pt7d2(Macro):
+    """Macro with a list of pair Motor,Float. Default value for both ParamRepeat elements.
+    Usages, ex.:
+    pt7d2 [[mot1 1][mot2 3]]
+    pt7d2 [mot1 1 mot2 3]
+    pt7d2 mot1 1 mot2 3
+    Using both default values, ex.:
+    pt7d1 [[][mot2 3][]] # at any repetition
+    pt7d1 [mot1 1 []] # only at the last repetition
+    """
+
+    param_def = [
+       [ 'm_p_pair', [ [ 'motor', Type.Motor, 'mot1', 'Motor to move'],
+                       [ 'pos',   Type.Float, 2, 'Position to move to'] ],
+         None, 'List of motor/position pairs']
+    ]
+    
+    def run(self, *args, **kwargs):
+        pass   
 
 class pt8(Macro):
     """Macro with a list of pair Motor,Float. The min and max elements have been
-    explicitly stated"""
+    explicitly stated.
+    Usages, ex.:
+    pt8 [[mot1 1][mot2 3]]
+    pt8 [mot1 1 mot2 3]
+    pt8 mot1 1 mot2 3    
+    """
     
     param_def = [
        [ 'm_p_pair', [ [ 'motor', Type.Motor, None, 'Motor to move'],
@@ -135,7 +238,12 @@ class pt8(Macro):
 class pt9(Macro):
     """Same as macro pt7 but with old style ParamRepeat. If you are writing
     a macro with variable number of parameters for the first time don't even
-    bother to look at this example since it is DEPRECATED."""
+    bother to look at this example since it is DEPRECATED.
+    Usages, ex.:
+    pt9 [[mot1 1][mot2 3]]
+    pt9 [mot1 1 mot2 3]
+    pt9 mot1 1 mot2 3
+    """
 
     param_def = [
        ['m_p_pair',
@@ -149,7 +257,11 @@ class pt9(Macro):
 
 class pt10(Macro):
     """Macro with list of numbers followed by a motor parameter. The repeat
-    parameter may be defined as first one."""
+    parameter may be defined as first one.
+    Usages, ex.:
+    pt10 [[1][3]] mot1
+    pt10 [1 3] mot1
+    """
 
     param_def = [
        ['numb_list', [['pos', Type.Float, None, 'value']], None, 'List of values'],
@@ -158,11 +270,15 @@ class pt10(Macro):
 
     def run(self, *args, **kwargs):
         pass
-
+  
 
 class pt11(Macro):
     """Macro with list of numbers followed by a motor parameter. The repeat
-    parameter may be defined as first one."""
+    parameter may be defined as first one.
+    Usages, ex.:
+    pt11 ct1 [[1][3]] mot1
+    pt11 ct1 [1 3] mot1
+    """
 
     param_def = [
        ['counter', Type.ExpChannel, None, 'Counter to count'],
@@ -176,7 +292,11 @@ class pt11(Macro):
 
 class pt12(Macro):
     """Macro with list of motors followed by list of numbers. Two repeat
-    parameters may defined."""
+    parameters may defined.
+    Usages, ex.:
+    pt12 [[1][3][4]] [[mot1][mot2]]
+    pt12 [1 3 4] [mot1 mot2]
+    """
 
     param_def = [
        ['numb_list', [['pos', Type.Float, None, 'value']], None, 'List of values'],
@@ -189,7 +309,11 @@ class pt12(Macro):
 
 class pt13(Macro):
     """Macro with list of motors groups, where each motor group is a list of 
-    motors. Repeat parameters may be defined as nested."""
+    motors. Repeat parameters may be defined as nested.
+    Usages, ex.:
+    pt13 [[[[mot1] [mot2]]][[[mot3] [mot4]]]]
+    pt13 [[mot1 mot2][mot3 mot4]]
+"""
 
     param_def = [
        ['motor_group_list',
@@ -200,7 +324,50 @@ class pt13(Macro):
     def run(self, *args, **kwargs):
         pass
 
+       
+class pt14(Macro):
+    """Macro with list of motors groups, where each motor group is a list of 
+    motors and a float. Repeat parameters may be defined as nested.
+    Usages, ex.:
+    pt14 [[[[mot61] [mot62]] 3]  [[[mot63]] 5] ]
+    pt14 [[mot61 mot62] 3  [mot63] 5]
+    """
 
+    param_def = [
+       ['motor_group_list',
+        [['motor_list', [['motor', Type.Motor, None, 'Motor to move']], None, 'List of motors'],
+         ['float', Type.Float, None, 'Number']],
+        None, 'Motor groups']
+    ]
+
+    def run(self, *args, **kwargs):
+        pass 
+       
+class pt14d(Macro):
+    """Macro with list of motors groups, where each motor group is a list of 
+    motors and a float. Repeat parameters may be defined as nested.
+    Default values can be used.
+    Usages, ex.:
+    pt14d [[[[mot61] [mot62]] 3]  [[[mot63]] 5] ]
+    pt14d [[mot61 mot62] 3  [mot63] 5]
+    Usages taken default values, ex.:
+    pt14d [[[[mot61] [mot62]]]  [[[mot63]] 5] ]
+    pt14d [[[[mot61] []]]  [[[mot63]] 5] ]
+    pt14d [[[]]  [[[mot63]] 5] ]
+    pt14d [[mot61 mot62] 3  [] 5]
+    pt14d [[mot61 mot62] 3  []] # only at the last repetition
+    """
+
+    param_def = [
+       ['motor_group_list',
+        [['motor_list', [['motor', Type.Motor, 'exp_dmy01', 'Motor to move']], None, 'List of motors'],
+         ['float', Type.Float, 33, 'Number']],
+        None, 'Motor groups']
+    ]
+
+    def run(self, *args, **kwargs):
+        pass
+    
 class twice(Macro):
     """A macro that returns a float that is twice its input. It also sets its 
     data to be a dictionary with 'in','out' as keys and value,result 
