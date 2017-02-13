bd6f05250987d42b5432d9a66b00b30037cb9579
mrosanes <mrosanes@cells.es>
2017-02-13 16:24:15 +0100

    Tests for encoding of macro parameters
    Insert tests to check the correct encoding/decoding of macro
    parameters.
    
    Using insertTest decorator, different parameter combinations
    (including repeat parameters) are tested.

---
+++ src/sardana/taurus/core/tango/sardana/test/res/params_definitions.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+
+##############################################################################
+##
+## This file is part of Sardana
+##
+## http://www.sardana-controls.org/
+##
+## Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+## Sardana is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Sardana is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with Sardana.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
+
+"""Resource file containing parameter definitions and related
+input and expected parameters, to be used in test_macro for testing
+encoding and decoding of macro parameters."""
+
+
+################################################################################
+
+params_definition_move = [
+    {'name': 'motor_pos_list',
+     'default_value': None,
+     'type': [
+        {'name': 'motor',
+         'default_value': "mot99",
+         'type': 'Moveable'},
+        {'name': 'pos',
+         'default_value': 100,
+         'type': 'Float'}
+        ]}
+    ]
+
+
+move_parameters_1 = [[["mot01", "0"]]]
+expected_result_1 = [[['mot01', "0"]]]
+
+move_parameters_2 = [[["mot01"]]]
+expected_result_2 = [[['mot01', "100"]]]
+
+move_parameters_3 = [[[]]]
+expected_result_3 = [[['mot99', "100"]]]
+
+### These case maybe should not be allowed. It introduces benefits but can
+### contain some ambiguity.
+move_parameters_4 = [[[[], "50"]]]
+expected_result_4 = [[['mot99', "50"]]]
+
+### These case maybe should not be allowed. It introduces benefits but can
+### contain some ambiguity.
+move_parameters_5 = [[['mot99', []]]]
+expected_result_5 = [[['mot99', "100"]]]
+
+### These case most probably should not be allowed. It introduces benefits but
+### can contain some ambiguity. To be studied in detail: it is important
+### that the information interpreted by Sardana is the one that the user
+### has the intention to give to Sardana. If a user can 'write' two different
+### behaviors in the same exact way, maybe Sardana will not execute the
+### user intention.
+move_parameters_6 = [[[['mot01'], ["40"]]]]
+expected_result_6 = [[['mot01', "40"]]]
+
+move_parameters_7 = [[["mot01", "0"], ["mot02", "5"], ["mot03", "10"]]]
+expected_result_7 = [[["mot01", "0"], ["mot02", "5"], ["mot03", "10"]]]
+
+move_parameters_8 = [[["mot01", "0"], [], ["mot03"]]]
+expected_result_8 = [[["mot01", "0"], ["mot99", "100"], ["mot03", "100"]]]
+
+
+################################################################################
+
+
+params_definition_float = [
+    {'name': 'float_list',
+     'default_value': None,
+     'type': [
+        {'name': 'float',
+         'default_value': 100,
+         'type': 'Float'}
+        ]}
+    ]
+
+
+# Old interface
+float_parameters_9 = ["1", "3", "15"]
+expected_result_9 = [["1", "3", "15"]]
+
+# New interface
+float_parameters_10 = [["1", "3", "15"]]
+expected_result_10 = [["1", "3", "15"]]
+
+float_parameters_11 = [[[], [], []]]
+expected_result_11 = [["100", "100", "100"]]
+
+float_parameters_12 = [[["1"], ["3"], ["15"]]]
+expected_result_12 = [["1", "3", "15"]]
+
+float_parameters_13 = [["3", [], "4"]]
+expected_result_13 = [["3", "100", "4"]]
+
+
+################################################################################
+
+
+
+
+
---
+++ src/sardana/taurus/core/tango/sardana/test/test_macro_paramrepeat.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+##############################################################################
+##
+## This file is part of Sardana
+##
+## http://www.sardana-controls.org/
+##
+## Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+## Sardana is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Sardana is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with Sardana.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
+
+"""Module with tests for macro related utils."""
+
+
+from taurus.external import unittest
+from taurus.test import insertTest
+from sardana.taurus.core.tango.sardana.macro import MacroNode
+from sardana.taurus.core.tango.sardana.test.res.params_definitions import *
+
+@insertTest(helper_name='verifyEncoding', macro_name="mv_like_macro",
+            param_def=params_definition_move, macro_params=move_parameters_1,
+            expected_params_list=expected_result_1)
+@insertTest(helper_name='verifyEncoding', macro_name="mv_like_macro",
+            param_def=params_definition_move, macro_params=move_parameters_2,
+            expected_params_list=expected_result_2)
+@insertTest(helper_name='verifyEncoding', macro_name="mv_like_macro",
+            param_def=params_definition_move, macro_params=move_parameters_3,
+            expected_params_list=expected_result_3)
+@insertTest(helper_name='verifyEncoding', macro_name="mv_like_macro",
+            param_def=params_definition_move, macro_params=move_parameters_4,
+            expected_params_list=expected_result_4)
+@insertTest(helper_name='verifyEncoding', macro_name="mv_like_macro",
+            param_def=params_definition_move, macro_params=move_parameters_5,
+            expected_params_list=expected_result_5)
+@insertTest(helper_name='verifyEncoding', macro_name="mv_like_macro",
+            param_def=params_definition_move, macro_params=move_parameters_6,
+            expected_params_list=expected_result_6)
+@insertTest(helper_name='verifyEncoding', macro_name="mv_like_macro",
+            param_def=params_definition_move, macro_params=move_parameters_7,
+            expected_params_list=expected_result_7)
+@insertTest(helper_name='verifyEncoding', macro_name="mv_like_macro",
+            param_def=params_definition_move, macro_params=move_parameters_8,
+            expected_params_list=expected_result_8)
+@insertTest(helper_name='verifyEncoding', macro_name="mv_like_macro",
+            param_def=params_definition_float, macro_params=float_parameters_9,
+            expected_params_list=expected_result_9)
+@insertTest(helper_name='verifyEncoding', macro_name="float_macro",
+            param_def=params_definition_float, macro_params=float_parameters_10,
+            expected_params_list=expected_result_10)
+@insertTest(helper_name='verifyEncoding', macro_name="float_macro",
+            param_def=params_definition_float, macro_params=float_parameters_11,
+            expected_params_list=expected_result_11)
+@insertTest(helper_name='verifyEncoding', macro_name="float_macro",
+            param_def=params_definition_float, macro_params=float_parameters_12,
+            expected_params_list=expected_result_12)
+@insertTest(helper_name='verifyEncoding', macro_name="float_macro",
+            param_def=params_definition_float, macro_params=float_parameters_13,
+            expected_params_list=expected_result_13)
+class ParamsTestCase(unittest.TestCase):
+
+    def verifyEncoding(self, macro_name, param_def, macro_params,
+                       expected_params_list):
+        """
+        Helper to verify the correct building of the parameters objects tree.
+        Verify that the list is recreated correctly from the parameters
+        objects tree.
+        :param macro_name: (str) name of the macro
+        :param param_def:   (list<dict>) macro parameters definition
+        :param macro_params: (list<str>) list of strings representing macro
+            parameters values.
+        :param expected_params_list: (list<str>) expected parameters list.
+        """
+
+        # Create the MacroNode with the inputs
+        node = MacroNode(name=macro_name, params_def=param_def)
+        node.fromList(macro_params)
+
+        output_params_list = node.toList()
+        output_params_list = [output_params_list[1]]
+        print(output_params_list)
+
+        msg = ("Parameters list is not encoded/decoded correctly. \n"
+               "expected: %s \n"
+               "received: %s" % (expected_params_list, output_params_list))
+        self.assertEqual(output_params_list, expected_params_list, msg)
+
+
+
+
