d6242bf03050117c674ef9211f171b6097d9dcd3
mrosanes <mrosanes@cells.es>
2017-02-16 17:08:01 +0100

    Fix bug when calling macros without parameters
    Macros called without parameters (e.g. lsm) are not working after
    implementation of the new param repeat interface.
    
    Fix bug when calling macros without parameters.

--- src/sardana/spock/spockms.py
+++ src/sardana/spock/spockms.py
@@ -610,15 +610,20 @@ class Reflector(object):
 def split_macro_parameters(parameters_s):
     macro_params = genutils.arg_split(parameters_s, posix=True)
     params_str = ''
-    for par in macro_params:
-        params_str = params_str + str(par) + ","
-    params_str = params_str[:-1] 
-    params_str = params_str.replace("[,", "[").replace(",]","]").replace("][", "],[")
-    macro_params = eval(params_str, globals(), Reflector())
-    if type(macro_params) == list:
-        new_params = []
-        new_params.append(macro_params)
-        macro_params = new_params
+
+    if len(macro_params) != 0:
+        for par in macro_params:
+            print(par)
+            params_str = params_str + str(par) + ","
+        params_str = params_str[:-1]
+        params_str = params_str.replace("[,", "[").replace(",]", "]").replace(
+            "][", "],[")
+        macro_params = eval(params_str, globals(), Reflector())
+        if type(macro_params) == list:
+            new_params = []
+            new_params.append(macro_params)
+            macro_params = new_params
+            
     # Convert tuple to list
     macro_params = list(macro_params)
     return macro_params
