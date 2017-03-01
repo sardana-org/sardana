5176aadc95cd631b9d2443d8786b3bf504b81553
Zbigniew Reszela <zreszela@cells.es>
2017-03-01 23:46:20 +0100

    Expose tests on sardana.tango.pool.test level

--- src/sardana/tango/pool/test/__init__.py
+++ src/sardana/tango/pool/test/__init__.py
@@ -26,3 +26,6 @@
 from base import (BasePoolTestCase, ControllerLoadsTestCase,
                   ControllerCreationTestCase, ElementCreationTestCase)
 from base_sartest import SarTestTestCase
+from .test_measurementgroup import *
+from .test_Motor import *
+from .test_persistence import *
