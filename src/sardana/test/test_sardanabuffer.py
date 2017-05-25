11255ece8d148d91a0f479e02204b6bdb9d1a172
zreszela <zreszela@cells.es>
2017-05-25 17:04:28 +0200

    Add more tests and asserts for SardanaBuffer

--- src/sardana/test/test_sardanabuffer.py
+++ src/sardana/test/test_sardanabuffer.py
@@ -27,7 +27,7 @@ from taurus.external.unittest import TestCase
 
 from sardana.sardanabuffer import SardanaBuffer
 
-class TestBuffer(TestCase):
+class TestPersistentBuffer(TestCase):
     """Unit tests for Buffer class"""
 
     def setUp(self):
@@ -38,5 +38,16 @@ class TestBuffer(TestCase):
         """Test extend method with a simple case of a list."""
         chunk = [4, 5, 6]
         self.buffer.extend(chunk)
-        self.assertEqual(self.buffer.get(0), 1)
-        self.assertEqual(self.buffer.get(5), 6)
+        self.assertEqual(self.buffer.get_value(0), 1)
+        self.assertEqual(self.buffer.get_value(5), 6)
+        self.assertEqual(len(self.buffer), 6)
+        self.assertEqual(len(self.buffer.last_chunk), 3)
+
+    def test_append(self):
+        """Test if append correctly fills the last_chunk as well as permanently
+        adds the value to the buffer.
+        """
+        self.buffer.append(1)
+        self.assertEqual(len(self.buffer), 4)
+        self.assertEqual(len(self.buffer.last_chunk), 1)
+
