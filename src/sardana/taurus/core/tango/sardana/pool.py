7dd514d98fa27d3e445282e23e3597d06619ca8d
Lukasz Dudek <lukasz.j.dudek@uj.edu.pl>
2017-06-01 16:06:36 +0200

    sardana-467 Corrected a typo in index in getting pool's element state.

--- src/sardana/taurus/core/tango/sardana/pool.py
+++ src/sardana/taurus/core/tango/sardana/pool.py
@@ -505,7 +505,7 @@ class PoolElement(BaseElement, TangoDevice):
         try:
             if Release.version_info[0] > 3:
                 # For Taurus 4
-                state = str(self.stateObj.read().rvalue).split('.')[0]
+                state = str(self.stateObj.read().rvalue).split('.')[1]
                 state.capitalize()
             else:
                 # For Taurus 3
