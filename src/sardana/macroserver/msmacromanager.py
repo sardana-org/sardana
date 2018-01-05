f32960f19feb07d7238749e9f45e80850608f185
teresa <tnunez@mail.desy.de>
2018-01-05 11:43:56 +0100

    Adding default filters

--- src/sardana/macroserver/msmacromanager.py
+++ src/sardana/macroserver/msmacromanager.py
@@ -77,7 +77,32 @@ from sardana.taurus.core.tango.sardana.macro import createMacroNode
 
 _BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+class LogFilter(logging.Filter):
+    # Possible fields: created, name, process, levelname, levelno, funcName, module, message = msg
+    # Ex.:
+    # created   -> 1515147905.34
+    # name      -> Door/issue102/1.Macro[ascan]
+    # process   -> 15638
+    # levelname -> OUTPUT
+    # levelno   -> 15
+    # funcName  -> output
+    # module    -> __init__
+    # message   -> 2         10        0         0         0         0      2.11293 
 
+    def __init__(self, param=None, field=None):
+        self.param = param
+        self.field = field
+
+    def filter(self, record):
+        allow = True
+        if self.param != "MacroExecutor" and "MacroExecutor" in record.name:
+            allow = True
+        elif self.field == "message":
+            allow = self.param not in record.msg
+        elif self.field == "name":
+            allow = self.param not in record.name
+        return allow
+        
 def islambda(f):
     """inspect doesn't come with islambda so I create one :-P"""
     return inspect.isfunction(f) and \
@@ -1339,6 +1364,8 @@ class MacroExecutor(Logger):
                     log_format = logging.Formatter(
                         "%(levelname)-8s %(asctime)s %(name)s: %(message)s")
                 macro_obj.fileHandler.setFormatter(log_format)
+                macro_obj.fileHandler.addFilter(LogFilter("[START]", "message"))
+                macro_obj.fileHandler.addFilter(LogFilter("[ END ]", "message"))
                 macro_obj.logger = macro_obj.getLogger()
                 macro_obj.logger.addHandler(macro_obj.fileHandler)
 
