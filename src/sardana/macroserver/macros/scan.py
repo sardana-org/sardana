4f6c9cdd5d00d6a49c743cb6541184ae8ec95699
Roberto Javier Homs Puron <rhoms@cells.es>
2018-01-23 10:12:53 +0100

    Fix typo error

--- src/sardana/macroserver/macros/scan.py
+++ src/sardana/macroserver/macros/scan.py
@@ -215,7 +215,7 @@ class aNscan(Hookable):
             raise ValueError('invalid value for mode %s' % mode)
         # _data is the default member where the Macro class stores the data.
         # Assign the date produced by GScan (or its subclasses) to it so all
-        # the Macro infrastrucutre related to the data works e.g. getter,
+        # the Macro infrastructure related to the data works e.g. getter,
         # property, etc. Ideally this should be done by the data setter
         # but this is available in the Macro class and we inherit from it
         # latter. More details in sardana-org/sardana#683.
@@ -725,7 +725,7 @@ class mesh(Macro, Hookable):
 
         # _data is the default member where the Macro class stores the data.
         # Assign the date produced by GScan (or its subclasses) to it so all
-        # the Macro infrastrucutre related to the data works e.g. getter,
+        # the Macro infrastructure related to the data works e.g. getter,
         # property, etc. Ideally this should be done by the data setter
         # but this is available in the Macro class and we inherit from it
         # latter. More details in sardana-org/sardana#683.
@@ -932,7 +932,7 @@ class fscan(Macro, Hookable):
 
         # _data is the default member where the Macro class stores the data.
         # Assign the date produced by GScan (or its subclasses) to it so all
-        # the Macro infrastrucutre related to the data works e.g. getter,
+        # the Macro infrastructure related to the data works e.g. getter,
         # property, etc. Ideally this should be done by the data setter
         # but this is available in the Macro class and we inherit from it
         # latter. More details in sardana-org/sardana#683.
@@ -1309,7 +1309,7 @@ class meshc(Macro, Hookable):
 
         # _data is the default member where the Macro class stores the data.
         # Assign the date produced by GScan (or its subclasses) to it so all
-        # the Macro infrastrucutre related to the data works e.g. getter,
+        # the Macro infrastructure related to the data works e.g. getter,
         # property, etc. Ideally this should be done by the data setter
         # but this is available in the Macro class and we inherit from it
         # latter. More details in sardana-org/sardana#683.
@@ -1636,7 +1636,7 @@ class meshct(Macro, Hookable):
                              extrainfodesc)
         # _data is the default member where the Macro class stores the data.
         # Assign the date produced by GScan (or its subclasses) to it so all
-        # the Macro infrastrucutre related to the data works e.g. getter,
+        # the Macro infrastructure related to the data works e.g. getter,
         # property, etc. Ideally this should be done by the data setter
         # but this is available in the Macro class and we inherit from it
         # latter. More details in sardana-org/sardana#683.
@@ -1725,7 +1725,7 @@ class timescan(Macro, Hookable):
 
         # _data is the default member where the Macro class stores the data.
         # Assign the date produced by GScan (or its subclasses) to it so all
-        # the Macro infrastrucutre related to the data works e.g. getter,
+        # the Macro infrastructure related to the data works e.g. getter,
         # property, etc. Ideally this should be done by the data setter
         # but this is available in the Macro class and we inherit from it
         # latter. More details in sardana-org/sardana#683.
