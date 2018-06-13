sardana-org/sardana

commit b52315b395e28246e3da38de3ad91cb03f66a35b

Relax restrictions in usage of repeat parameters in spock
Spock syntax allows to use macros with only one repeat parameter which is
defined at the end. Furthermore no nested repeat parameters are allowed.
Remove this validation so it will be possible to extend the spock syntax
for more cases.

src/sardana/taurus/core/tango/sardana/macro.py
@@ -1301,18 +1301,7 @@ def createMacroNode(macro_name, params_def, macro_params):
 
     if not new_interface:
         param_nodes = macro_node.params()
-        contain_param_repeat = False
         len_param_nodes = len(param_nodes)
-        for i, param_node in enumerate(param_nodes):
-            if isinstance(param_node, RepeatParamNode):
-                if contain_param_repeat:
-                    msg = "Only one repeat parameter is allowed"
-                    raise Exception(msg)
-                if i < len_param_nodes - 1:
-                    msg = "Repeat parameter must be the last one"
-                    raise Exception(msg)
-        # If ParamRepeat only one and as last parameter
-        # this ignores raw_parameters which exceeds the param_def
         for param_node, param_raw in zip(param_nodes, macro_params):
             if isinstance(param_node, SingleParamNode):
                 param_node.setValue(param_raw)

commit c3e6538e6f33aa1c933c86794dad7890c30d5d3f

Fix bug and use proper index of the last parameter
Commit b52315b accidentally remove the i variable. Fix it.

src/sardana/taurus/core/tango/sardana/macro.py
@@ -1332,9 +1332,10 @@ def createMacroNode(macro_name, params_def, macro_params):
             else:
                 params_info = param_node.paramsInfo()
                 params_info_len = len(params_info)
+                last_param_idx = len_param_nodes - 1
                 rep = 0
                 mem = 0
-                rest_raw = macro_params[i:]
+                rest_raw = macro_params[last_param_idx:]
                 for member_raw in rest_raw:
                     repeat_node = param_node.child(rep)
                     # Add a new repeat node. This is needed when the raw