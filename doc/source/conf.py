5a532b161f86b5af2a0b6b67a7633ff2e083964a
Carlos Pascual <cpascual@cells.es>
2018-07-12 15:41:32 +0200

    Avoid using sphinx.ext.pngmath
    The `sphinx.ext.pngmath` sphinx extension is deprecated. use `imgpath`

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -63,7 +63,7 @@ sys.path.append(os.path.abspath('sphinxext'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.pngmath',
+extensions = ['sphinx.ext.imgmath',
               'sphinx.ext.autosummary',
               'sphinx.ext.autodoc',
               'sphinx.ext.doctest',
