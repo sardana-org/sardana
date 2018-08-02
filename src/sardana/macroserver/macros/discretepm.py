9c764e33e840b045cde3be39dec4e8c78843735e
Jordi Andreu <jandreu@cells.es>
2018-08-02 15:42:46 +0200

    Add license header

--- src/sardana/macroserver/macros/discretepm.py
+++ src/sardana/macroserver/macros/discretepm.py
@@ -1,3 +1,28 @@
+##############################################################################
+##
+# This file is part of Sardana
+##
+# http://www.sardana-controls.org/
+##
+# Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+# Sardana is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+##
+# Sardana is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+##
+# You should have received a copy of the GNU Lesser General Public License
+# along with Sardana.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
+
+""" Discrete pseudo motor controller configuration related macros"""
+
 from sardana.macroserver.macro import Macro, Type
 import json
 import math
