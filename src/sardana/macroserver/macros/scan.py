2f99413d71d32e0f8b6a8c5390ce8433a010b1ec
Roberto Javier Homs Puron <rhoms@cells.es>
2018-08-07 15:12:57 +0200

    Returns as intervals estimation the number of waypoints

--- src/sardana/macroserver/macros/scan.py
+++ src/sardana/macroserver/macros/scan.py
@@ -1802,7 +1802,7 @@ class meshct(Macro, Hookable):
         return 0.0
 
     def getIntervalEstimation(self):
-        return int((self.nr_intervs[0] + 1) * (self.nr_intervs[1] + 1) - 1)
+        return len(self.waypoints)
 
     def _fill_missing_records(self):
         # fill record list with dummy records for the final padding
