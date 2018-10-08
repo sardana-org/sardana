10e40f8f6803326cf8388a3d052c17c54559e092
Daniel Roldan <droldan@cells.es>
2018-10-08 11:26:38 +0200

    Add prepare method in MeasurementGroup Taurus ext

--- src/sardana/taurus/core/tango/sardana/pool.py
+++ src/sardana/taurus/core/tango/sardana/pool.py
@@ -1795,8 +1795,8 @@ class MeasurementGroup(PoolElement):
                 self.debug("stopped")
             raise e
 
-    def prepare(self, synchronization, nr_of_starts=1):
-        pass
+    def prepare(self):
+        self.command_inout("Prepare")
 
     def go(self, *args, **kwargs):
         start_time = time.time()
