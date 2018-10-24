9a6f0e03a2f4812f4419c43e5cf6e1f6f89a9eca
Roberto Javier Homs Puron <rhoms@cells.es>
2018-10-24 15:25:03 +0200

    Refactor configuration classes
    Refactor classes to use them with the synchronizer controllers and
    channels

--- src/sardana/pool/poolmeasurementgroup.py
+++ src/sardana/pool/poolmeasurementgroup.py
@@ -128,11 +128,6 @@ class ConfigurationItem(object):
     def __init__(self, element, conf=None):
         self._element = weakref.ref(element)
 
-        if conf is None:
-            conf = {'acq_synch': None}
-        else:
-            conf['acq_synch'] = None
-
         if conf is not None:
             self.__dict__.update(conf)
 
@@ -165,24 +160,6 @@ class ControllerConfiguration(ConfigurationItem):
 
         ConfigurationItem.__init__(self, element, conf)
 
-    def get_config(self):
-        config = {}
-        # the external controllers: Tango attributes does not have timer and
-        #  monitor
-        config['timer'] = self.timer.full_name
-        config['monitor'] = self.monitor.full_name
-
-        if isinstance(self.synchronizer, (str, unicode)):
-            synchronizer = self.synchronizer
-        else:
-            synchronizer = self.synchronizer.full_name
-        config['synchronizer'] = synchronizer
-        config['synchronization'] = self.synchronization
-        config['channels'] = {}
-        for ch_item in self._channels:
-            config['channels'][ch_item.full_name] = ch_item.get_config()
-        return config
-
     def add_channel(self, channel_item):
         self._channels.append(channel_item)
         if channel_item.enabled:
@@ -194,6 +171,17 @@ class ControllerConfiguration(ConfigurationItem):
             if self._channels_disabled is None:
                 self._channels_disabled = []
             self._channels_disabled.append(channel_item)
+
+    def update_state(self):
+        self.enabled = False
+        self._channels_enabled = []
+        self._channels_disabled = []
+        for channel_item in self._channels:
+            if channel_item.enabled:
+                self.enabled = True
+                self._channels_enabled.append(channel_item)
+            else:
+                self._channels_disabled.append(channel_item)
 
     def get_channels(self, enabled=None):
         if enabled is None:
@@ -208,25 +196,6 @@ class ChannelConfiguration(ConfigurationItem):
     """Configuration: 'index', 'enabled', 'output', 'plot_type', 'plot_axes',
                       'label', 'scale', 'plot_color'"""
 
-    def get_config(self):
-        config = {}
-        config['index'] = self.index
-        config['name'] = self.name
-        config['full_name'] = self.full_name
-        config['source'] = self.source
-        config['enabled'] = self.enabled
-        config['label'] = self.label
-        config['ndim'] = self.ndim
-        config['output'] = self.output
-        config['plot_type'] = self.plot_type
-        config['plot_axes'] = self.plot_axes
-        config['conditioning'] = self.conditioning
-        config['normalization'] = self.normalization
-        config['data_type'] = self.data_type
-        config['data_units'] = self.data_units
-        config['nexus_path'] = self.nexus_path
-        config['shape'] = self.shape
-        return config
 
 
 class MeasurementConfiguration(object):
