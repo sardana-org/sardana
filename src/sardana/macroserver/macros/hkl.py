e5c4e16add3385dd8765893470575bed728679b1
teresa <tnunez@mail.desy.de>
2019-02-04 13:22:38 +0100

    Adding license and correcting typo

--- src/sardana/macroserver/macros/hkl.py
+++ src/sardana/macroserver/macros/hkl.py
@@ -1,3 +1,25 @@
+##############################################################################
+##
+# This file is part of Sardana
+##
+# http://www.sardana-controls.org/
+##
+# Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+# Sardana is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+##
+# Sardana is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+##
+# You should have received a copy of the GNU Lesser General Public License
+# along with Sardana.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
 
 """
     Macro library containning diffractometer related macros for the macros
@@ -13,7 +35,7 @@
 # using getDevice. However this getter seems to accept only the elements names
 # and not the full names.
 
-__all__ = ["addreflexion", "affine", "br", "ca", "caa", "ci", "computeub",
+__all__ = ["addreflection", "affine", "br", "ca", "caa", "ci", "computeub",
            "freeze", "getmode", "hklscan", "hscan", "kscan", "latticecal",
            "loadcrystal", "lscan", "newcrystal", "or0", "or1", "orswap",
            "pa", "savecrystal", "setaz", "setlat", "setmode", "setor0",
