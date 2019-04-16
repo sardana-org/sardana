0ec06917490fd8bdecf903e51c18953a6c1c599f
zreszela <zreszela@cells.es>
2019-04-16 11:12:37 +0200

    Add test of VDS creation in h5 recorder

---
+++ src/sardana/macroserver/recorders/test/test_h5storage.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+##############################################################################
+##
+# This file is part of Sardana
+##
+# http://www.sardana-controls.org/
+##
+# Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+# Sardana is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+##
+# Sardana is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+##
+# You should have received a copy of the GNU Lesser General Public License
+# along with Sardana.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
+
+"""This module contains tests for HDF5 recorders."""
+
+import os
+import tempfile
+from datetime import datetime
+
+import h5py
+import numpy
+from taurus.external.unittest import TestCase, expectedFailure
+
+from sardana.macroserver.scan import ColumnDesc
+from sardana.macroserver.recorders.h5storage import NXscanH5_FileRecorder
+
+COL1_NAME = "col1"
+
+
+class RecordList(dict):
+
+    def __init__(self, env):
+        self._env = env
+
+    def getEnviron(self):
+        return self._env
+
+
+class Record(object):
+
+    def __init__(self, data, recordno=0):
+        self.data = data
+        self.recordno = recordno
+
+
+class TestNXscanH5_FileRecorder(TestCase):
+
+    def setUp(self):
+        self.dir_name = tempfile.gettempdir()
+        self.path = os.path.join(self.dir_name, "test_vds.h5")
+        try:
+            os.remove(self.path)  # remove file just in case
+        except OSError, e:
+            pass
+
+        self.env = {
+            "serialno": 0,
+            "starttime": None,
+            "title": "test",
+            "user": "user",
+            "datadesc": None,
+            "endtime": None
+        }
+        self.record_list = RecordList(self.env)
+
+    @expectedFailure
+    def test_VDS(self):
+        """Test creation of VDS when channel reports URIs (str) of h5file
+        scheme in a simulated sardana scan (3 points).
+        """
+        nb_records = 3
+        # create partial files
+        part_file_name_patter = "test_vds_part{0}.h5"
+        part_file_paths = []
+        for i in range(nb_records):
+            path = os.path.join(self.dir_name,
+                                part_file_name_patter.format(i))
+            part_file_paths.append(path)
+            part_file = h5py.File(path, "w")
+            img = numpy.array([[i, i], [i, i]])
+            part_file.create_dataset("dataset", data=img)
+            part_file.flush()
+            part_file.close()
+        try:
+            # create description of channel data
+            data_desc = [
+                ColumnDesc(name=COL1_NAME, dtype="str", shape=tuple())
+            ]
+            self.env["datadesc"] = data_desc
+
+            # simulate sardana scan
+            recorder = NXscanH5_FileRecorder(filename=self.path)
+            self.env["starttime"] = datetime.now()
+            recorder._startRecordList(self.record_list)
+            for i in range(nb_records):
+                record = Record({COL1_NAME: part_file_name_patter.format(i)})
+                recorder._writeRecord(record)
+            self.env["endtime"] = datetime.now()
+            recorder._endRecordList(self.record_list)
+
+            # assert if reading datasets from the sardana file access to the
+            # dataset of the partial files
+            file_ = h5py.File(self.path)
+            for i in range(nb_records):
+                expected_img = numpy.array([[i, i], [i, i]])
+                img = file_["entry0"]["measurement"][COL1_NAME][i]
+                msg = "VDS extracted image does not match"
+                # TODO: check if this assert works well
+                numpy.testing.assert_array_equal(img, expected_img, msg)
+        finally:
+            # remove partial files
+            for path in part_file_paths:
+                os.remove(path)
+
+    def tearDown(self):
+        try:
+            os.remove(self.path)
+        except OSError:
+            pass
