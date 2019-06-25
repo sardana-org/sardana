7b2142f0233817af46b3e1dd2c8519912127072e
Tim Schoof <tim.schoof@desy.de>
2019-06-25 17:48:10 +0200

    Add QtSpock requirements

--- setup.py
+++ setup.py
@@ -64,6 +64,9 @@ install_requires = [
     'lxml>=2.3'
 ]
 
+test_require = [
+    'mock; python_version<"3.3"'
+]
 
 console_scripts = [
     "MacroServer = sardana.tango.macroserver:main",
@@ -123,5 +126,6 @@ setup(name='sardana',
       provides=provides,
       requires=requires,
       install_requires=install_requires,
+      test_require=test_require,
       test_suite='sardana.test.testsuite.get_sardana_unitsuite',
       )
