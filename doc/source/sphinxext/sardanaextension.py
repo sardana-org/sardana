3e1fd8ea4fc743b27f0793e792ec2f8a7aac1731
zreszela <zreszela@cells.es>
2019-07-21 22:31:45 +0200

    2to3: migrate documentation

--- doc/source/sphinxext/sardanaextension.py
+++ doc/source/sphinxext/sardanaextension.py
@@ -65,9 +65,9 @@ def process_param(line):
                 new_lines.append('%s:type %s: %s' %
                                  (prefix, param_name, klass))
         new_lines.append('%s:param %s: %s' % (prefix, param_name, desc))
-    except Exception, e:
-        print "Sardana sphinx extension: Not able to process param: '%s'" % line
-        print "      Reason:", str(e)
+    except Exception as e:
+        print("Sardana sphinx extension: Not able to process param: '%s'" % line)
+        print("      Reason:", str(e))
         new_lines.append(line)
     return new_lines
 
@@ -85,9 +85,9 @@ def process_return(line):
                 desc = desc[pos + 1:]
                 new_lines.append('%s:rtype: %s' % (prefix, klass))
         new_lines.append('%s:return: %s' % (prefix, desc))
-    except Exception, e:
-        print "Sardana sphinx extension: Not able to process 'return': '%s'" % line
-        print "      Reason:", str(e)
+    except Exception as e:
+        print("Sardana sphinx extension: Not able to process 'return': '%s'" % line)
+        print("      Reason:", str(e))
         new_lines.append(line)
     return new_lines
 
@@ -105,9 +105,9 @@ def process_raise(line):
                 klass = "(" + process_type(elem_type, obj_type='exc') + ")"
                 desc = desc[pos + 1:]
         new_lines.append('%s:raise: %s %s' % (prefix, klass, desc))
-    except Exception, e:
-        print "Sardana sphinx extension: Not able to process 'raise': '%s'" % line
-        print "      Reason:", str(e)
+    except Exception as e:
+        print("Sardana sphinx extension: Not able to process 'raise': '%s'" % line)
+        print("      Reason:", str(e))
         new_lines.append(line)
     return new_lines
 
@@ -192,7 +192,8 @@ def process_signature(app, what, name, obj, options, signature, return_annotatio
     if hasattr(obj, "__wrapped__"):
         if what == "method":
             from taurus.core.util.wrap import wrapped
-            obj = wrapped(obj)
+            # import pdb; pdb.set_trace()
+            # obj = wrapped(obj)
             signature = _format_method_args(obj)
             return signature, return_annotation
 
