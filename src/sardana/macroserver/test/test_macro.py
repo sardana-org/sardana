36116d38d02b36e9b33336bc635f30e92526a096
zreszela <zreszela@cells.es>
2021-03-15 11:42:48 +0100

    test: enhance Hookable test to get unexisting hook place

--- src/sardana/macroserver/test/test_macro.py
+++ src/sardana/macroserver/test/test_macro.py
@@ -4,5 +4,8 @@ from sardana.macroserver.macro import Hookable
 def test_Hookable():
     hookable = Hookable()
     assert len(hookable.hooks) == 0
+    hooks = hookable.getHooks("unexisting-hook-place")
+    assert len(hooks) == 0
     hookable.hooks = []
     assert len(hookable.hooks) == 0
+    hooks = hookable.getHooks("unexisting-hook-place")
