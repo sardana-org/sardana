b83383311c4021e995965fa7fa28321cc41cebee
zreszela <zreszela@cells.es>
2021-10-22 15:45:29 +0200

    test: add more test scenarios for MotionPath

--- src/sardana/util/test/test_motion.py
+++ src/sardana/util/test/test_motion.py
@@ -57,9 +57,28 @@ def test_motor():
     assert m.getDecelerationTime() == decel_time
 
 
-@pytest.mark.parametrize("start,end,expected_at_max_vel_displacement", [(0, 1, 0.9), (1, 0, 0.9), (0.001, 0, 0)])
-def test_motion_path(start, end, expected_at_max_vel_displacement):
-    motor = Motor(min_vel=0, max_vel=1, accel_time=0.1, decel_time=0.1)
+def motor01():
+    return Motor(min_vel=0, max_vel=1, accel_time=0.1, decel_time=0.1)
+
+
+def motor02():
+    return Motor(min_vel=0, max_vel=1, accel_time=0.001, decel_time=0.001)
+
+
+def motor03():
+    return Motor(min_vel=1, max_vel=2, accel_time=1, decel_time=2)
+
+
+@pytest.mark.parametrize(
+    "motor,start,end,expected_at_max_vel_displacement", [
+    (motor01(), 0, 1, 0.9),
+    (motor01(), 1, 0, 0.9),
+    (motor01(), 0.1, 0, 0),
+    (motor02(), 0.1, 0, 0.099),
+    (motor03(), 0, 0.25, 0)
+    ]
+)
+def test_motion_path(motor, start, end, expected_at_max_vel_displacement):
     motion_path = MotionPath(motor, start, end)
     motion_path.info()
     assert motion_path.at_max_vel_displacement == expected_at_max_vel_displacement
